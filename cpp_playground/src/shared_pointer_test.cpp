d1ae69fc6584fcc3e225d76daf13820bd22e5590
r2road <studior2road@gmail.com>
2021-02-13 10:20:44 +0900

    update : shared_pointer_test

--- cpp_playground/src/shared_pointer_test.cpp
+++ cpp_playground/src/shared_pointer_test.cpp
@@ -11,14 +11,18 @@ namespace
 
 		std::shared_ptr<int> test_sp( new int( 10 ) );
 		std::cout << "\t" << "+ Make Sp" << std::endl;
-		std::cout << "\t\t" << *test_sp << std::endl;
+		std::cout << "\t\t" << "+ std::shared_ptr<int> test_sp( new int( 10 ) )" << std::endl;
+		std::cout << "\t\t\t" << *test_sp << std::endl;
+		std::cout << std::endl;
 
 		const auto test_sp_2 = test_sp;
 		std::cout << "\t" << "+ Copy To Const Sp" << std::endl;
+		std::cout << std::endl;
 
 		*test_sp_2 = 11;
 		std::cout << "\t" << "+ Change Value with Const Sp" << std::endl;
 		std::cout << "\t\t" << *test_sp_2 << std::endl;
+		std::cout << std::endl;
 	}
 
 	void Test2()
@@ -27,7 +31,12 @@ namespace
 
 		std::shared_ptr<const int> const_test_sp( new int( 10 ) );
 		std::cout << "\t" << "+ Make Sp Has Const Value" << std::endl;
-		std::cout << "\t\t" << *const_test_sp << std::endl;
+		std::cout << "\t\t" << "+ std::shared_ptr<const int> test_sp( new int( 10 ) )" << std::endl;
+		std::cout << "\t\t\t" << *const_test_sp << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "\t" << "+ Failed : Change Value" << std::endl;
+		std::cout << std::endl;
 
 		//*const_test_sp = 11;
 		//std::cout << "\t" << "+ Change Value with Sp Has Const Value" << std::endl;
