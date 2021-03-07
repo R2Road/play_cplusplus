4fd0baeb6082f5d95a8e9ca0ab84a8dc8ac87f1a
r2road <studior2road@gmail.com>
2021-03-07 11:21:29 +0900

    ...

--- cpp_playground/src/random_test.h
+++ cpp_playground/src/random_test.h
@@ -5,7 +5,7 @@ namespace random_test
 	class  StatusSaveAndLoad
 	{
 	public:
-		static const char* GetTitle() { return "StatusSaveAndLoad"; }
+		static const char* GetTitle() { return "Random : Status Save And Load"; }
 		static void Do();
 	};
 }
