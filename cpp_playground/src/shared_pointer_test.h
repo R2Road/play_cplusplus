484f094363a81e26ee54caab8a962bf8e1db438e
r2road <studior2road@gmail.com>
2021-03-07 11:22:19 +0900

    ...

--- cpp_playground/src/shared_pointer_test.h
+++ cpp_playground/src/shared_pointer_test.h
@@ -5,7 +5,7 @@ namespace shared_pointer_test
 	class SharedPtr
 	{
 	public:
-		static const char* GetTitle() { return "Shared Pointer"; }
+		static const char* GetTitle() { return "Memory : Shared Pointer"; }
 		static void Do();
 	};
 }
