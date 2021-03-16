2ee2b94f535a9cb7fbacef40c56f3560b2d12d68
r2road <studior2road@gmail.com>
2021-03-16 13:49:01 +0900

    update : optional test - Basic

--- cpp_playground/src/optional_test.cpp
+++ cpp_playground/src/optional_test.cpp
@@ -3,18 +3,35 @@
 #include <iostream>
 #include <optional>
 
+namespace
+{
+	struct TestStruct
+	{
+		TestStruct() { std::cout << "- Call : TestStruct::TestStruct()"; }
+		TestStruct( const int i ) : I( i ) { std::cout << "- Call : TestStruct::TestStruct( const int i )"; }
+		TestStruct( const TestStruct& ) { std::cout << "- Call : TestStruct::TestStruct( const TestStruct& )"; }
+
+		int I = 111;
+	};
+
+	static const TestStruct test_structs[2] = { { 222 }, { 333 } };
+
+	std::optional<TestStruct> GetTestStruct( const std::size_t index )
+	{
+		if( 2 > index )
+		{
+			return test_structs[index];
+		}
+		else
+		{
+			return std::nullopt;
+		}
+	}
+}
 namespace optional_test
 {
 	void Basic::Do()
 	{
-		struct TestStruct
-		{
-			TestStruct() { std::cout << "- Call : TestStruct::TestStruct()"; }
-			TestStruct( const TestStruct& ) { std::cout << "- Call : TestStruct::TestStruct( const TestStruct& )"; }
-
-			int i = 0;
-		};		
-
 		std::cout << "== Optional : Basic ==" << std::endl;
 
 
@@ -46,6 +63,41 @@ namespace optional_test
 		std::cout << std::endl;
 		std::cout << "\t\t- Call : has_value()" << std::endl;
 		std::cout << "\t\t\t" << op_ts.has_value() << std::endl;
+
+
+		std::cout << std::endl << std::endl;
+
+
+		std::cout << "\t+ Value() " << std::endl;
+		std::cout << "\t\t- op_ts.value().I;" << std::endl;
+		std::cout << "\t\t\t" << op_ts.value().I << std::endl;
+
+
+		std::cout << std::endl << std::endl;
+
+
+		bool has_value = false;
+		std::cout << "\t+ GetTestStruct() " << std::endl;
+		{
+			std::cout << "\t\t- GetTestStruct( 0 );" << std::endl;
+			std::cout << "\t\t\t";
+			const auto result = GetTestStruct( 0 );
+			std::cout << "\t\t- Call : has_value()" << std::endl;
+			std::cout << "\t\t\t" << result.has_value() << std::endl;
+		}
+		{
+			std::cout << "\t\t- GetTestStruct( 1 );" << std::endl;
+			std::cout << "\t\t\t";
+			const auto result = GetTestStruct( 1 );
+			std::cout << "\t\t- Call : has_value()" << std::endl;
+			std::cout << "\t\t\t" << result.has_value() << std::endl;
+		}
+		{
+			std::cout << "\t\t- GetTestStruct( 2 );" << std::endl;
+			const auto result = GetTestStruct( 2 );
+			std::cout << "\t\t- Call : has_value()" << std::endl;
+			std::cout << "\t\t\t" << result.has_value() << std::endl;
+		}
 
 
 		std::cout << std::endl << std::endl;
