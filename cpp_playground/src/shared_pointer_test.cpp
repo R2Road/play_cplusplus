47dc5a78eb7a162539d4476dcf8e54fdcb8274f7
r2road <studior2road@gmail.com>
2021-03-28 06:19:21 +0900

    ...

--- cpp_playground/src/shared_pointer_test.cpp
+++ cpp_playground/src/shared_pointer_test.cpp
@@ -7,32 +7,39 @@ namespace
 {
 	void Test1()
 	{
-		std::cout << "== Shared Pointer ==" << std::endl;
+		std::cout << "== Shared Pointer ==" << std::endl << std::endl;
 
+		std::cout << "\t" << "+ Make Sp" << std::endl;
+		std::cout << "\t\t" << "std::shared_ptr<int> test_sp( new int( 10 ) )" << std::endl;
 		std::shared_ptr<int> test_sp( new int( 10 ) );
-			
-		std::cout << "\t\t" << "+ std::shared_ptr<int> test_sp( new int( 10 ) )" << std::endl;
-		std::cout << "\t\t\t" << *test_sp << std::endl;
-		std::cout << std::endl;
 
-		const auto test_sp_2 = test_sp;
+		std::cout << "\t\t\t - Print : " << *test_sp << std::endl << std::endl;
+
+
 		std::cout << "\t" << "+ Copy To Const Sp" << std::endl;
+		std::cout << "\t\t" << "const auto test_sp_2 = test_sp;" << std::endl;
+		const auto test_sp_2 = test_sp;
+
 		std::cout << std::endl;
 
-		*test_sp_2 = 11;
+
 		std::cout << "\t" << "+ Change Value with Const Sp" << std::endl;
-		std::cout << "\t\t" << *test_sp_2 << std::endl;
+		std::cout << "\t\t" << "*test_sp_2 = 11;" << std::endl;
+		*test_sp_2 = 11;
+
+		std::cout << "\t\t\t - Print : " << *test_sp_2 << std::endl;
 		std::cout << std::endl;
 	}
 
 	void Test2()
 	{
-		std::cout << "== Shared Pointer Has Const Value ==" << std::endl;
+		std::cout << "== Shared Pointer Has Const Value ==" << std::endl << std::endl;
 
-		std::shared_ptr<const int> const_test_sp( new int( 10 ) );
 		std::cout << "\t" << "+ Make Sp Has Const Value" << std::endl;
-		std::cout << "\t\t" << "+ std::shared_ptr<const int> test_sp( new int( 10 ) )" << std::endl;
-		std::cout << "\t\t\t" << *const_test_sp << std::endl;
+		std::cout << "\t\t" << "std::shared_ptr<const int> test_sp( new int( 10 ) )" << std::endl;
+		std::shared_ptr<const int> const_test_sp( new int( 10 ) );
+
+		std::cout << "\t\t\t - Print : " << *const_test_sp << std::endl;
 		std::cout << std::endl;
 
 		std::cout << "\t" << "+ Failed : Change Value" << std::endl;
