e606fcff66519d7b18637b4b90aee0ec472c8733
r2road <studior2road@gmail.com>
2021-03-28 07:51:59 +0900

    update : variadic template test - SubtractValues

--- cpp_playground/src/variadic_template_test.cpp
+++ cpp_playground/src/variadic_template_test.cpp
@@ -208,58 +208,14 @@ namespace variadic_template_test
 				std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 );" << std::endl;
 
 				std::cout << "\t\t\t" << subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << std::endl;
-
-				std::cout << "\t\t\t" << "?????????????????????" << std::endl;
 			}
 
 			std::cout << std::endl;
 
 			{
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 10, 1, 1 );" << std::endl;
+				std::cout << "\t\t" << "Test : ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) );" << std::endl;
 
-				std::cout << "\t\t\t" << subtract_args_with_fold_1( 10, 1, 1 ) << std::endl;
-
-				std::cout << "\t\t\t" << "?????????????????????" << std::endl;
-			}
-
-			std::cout << std::endl;
-
-			{
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 1, 1, 1 );" << std::endl;
-
-				std::cout << "\t\t\t" << subtract_args_with_fold_1( 1, 1, 1 ) << std::endl;
-
-				std::cout << "\t\t\t" << "?????????????????????" << std::endl;
-			}
-
-			std::cout << std::endl;
-
-			{
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 1, 1, 2 );" << std::endl;
-
-				std::cout << "\t\t\t" << subtract_args_with_fold_1( 1, 1, 2 ) << std::endl;
-
-				std::cout << "\t\t\t" << "?????????????????????" << std::endl;
-			}
-
-			std::cout << std::endl;
-
-			{
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 1, 1, 2, 3 );" << std::endl;
-
-				std::cout << "\t\t\t" << subtract_args_with_fold_1( 1, 1, 2, 3 ) << std::endl;
-
-				std::cout << "\t\t\t" << "?????????????????????" << std::endl;
-			}
-
-			std::cout << std::endl;
-
-			{
-				std::cout << "\t\t" << "Test : ( 100 - 1 ) + ( 2 - 3 ) + ( 4 - 5 ) + ( 6 - 7 ) + ( 8 - 9 );" << std::endl;
-
-				std::cout << "\t\t\t" << ( ( 100 - 1 ) + ( 2 - 3 ) + ( 4 - 5 ) + ( 6 - 7 ) + ( 8 - 9 ) ) << std::endl;
-
-				std::cout << "\t\t\t" << "Maybe????" << std::endl;
+				std::cout << "\t\t\t" << ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) ) << std::endl;
 			}
 		}
 
