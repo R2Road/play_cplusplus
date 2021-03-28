b59a57a1850f91490b6bc745d95d212d0d3e25d8
r2road <studior2road@gmail.com>
2021-03-29 00:16:33 +0900

    update : tuple_test - Basic

--- cpp_playground/src/tuple_test.cpp
+++ cpp_playground/src/tuple_test.cpp
@@ -34,6 +34,24 @@ namespace tuple_test
 
 
 		{
+			std::cout << "\t+ std::variant<int, std::string, bool> v{ 2, ""park"", 'b' };" << std::endl;
+			std::tuple<int, std::string, char> v{ 2, "park", 'b' };
+
+			std::cout << "\t\t- Call : std::get<0>( v );" << std::endl;
+			std::cout << "\t\t\t" << std::get<0>( v ) << std::endl;
+
+			std::cout << "\t\t- Call : std::get<1>( v );" << std::endl;
+			std::cout << "\t\t\t" << std::get<1>( v ) << std::endl;
+
+			std::cout << "\t\t- Call : std::get<2>( v );" << std::endl;
+			std::cout << "\t\t\t" << std::get<2>( v ) << std::endl;
+		}
+
+
+		std::cout << std::endl << std::endl;
+
+
+		{
 			std::cout << "\t+ auto v = std::make_tuple( 10, ""Kim"", true );" << std::endl;
 			auto v = std::make_tuple( 10, "Kim", true );
 
