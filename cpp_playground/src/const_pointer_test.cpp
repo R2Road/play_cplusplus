cd507ee59d7c2d8f6f83e7a7252e1a589fedfcfb
r2road <studior2road@gmail.com>
2021-03-30 00:34:00 +0900

    update : const_pointer_test - Basic

--- cpp_playground/src/const_pointer_test.cpp
+++ cpp_playground/src/const_pointer_test.cpp
@@ -39,6 +39,27 @@ namespace const_pointer_test
 			*temp_pointer_const = temp_int_2;
 		}
 
+
+
 		std::cout << std::endl << std::endl;
+
+
+
+		std::cout << "\t" << "const int temp_const_int_1 = 0;" << std::endl;
+		const int temp_const_int_1 = 0;
+
+		std::cout << std::endl;
+
+		{
+			std::cout << "\t" << "OK : const int* temp_pointer_const = &temp_const_int_1;" << std::endl;
+			const int* temp_pointer_const = &temp_const_int_1;
+		}
+
+		std::cout << std::endl;
+
+		{
+			std::cout << "\t" << "Error : int* const temp_pointer_const = &temp_const_int_1;" << std::endl;
+			//int* const temp_pointer_const = &temp_const_int_1;
+		}
 	}
 }
