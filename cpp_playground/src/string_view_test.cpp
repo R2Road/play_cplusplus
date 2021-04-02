e38233c7805a307165f2b335a0b831d9bd853c07
r2road <studior2road@gmail.com>
2021-04-02 11:50:20 +0900

    update : string_view_test - Basic

--- cpp_playground/src/string_view_test.cpp
+++ cpp_playground/src/string_view_test.cpp
@@ -1,6 +1,7 @@
 #include "string_view_test.h"
 
 #include <iostream>
+#include <string>
 #include <string_view>
 
 namespace stringview_test
@@ -10,10 +11,23 @@ namespace stringview_test
 		std::cout << "== StringView ==" << std::endl << std::endl;
 
 		{
-			std::cout << "\t" << "+ std::string_view sv = ""String View Test"";" << std::endl << std::endl;
-			std::string_view sv = "String View Test";
+			std::cout << "\t" << "+ std::string_view sv = ""String View Test_1"";" << std::endl << std::endl;
+			std::string_view sv = "String View Test_1";
 
-			std::cout << "\t\t - Print : " << sv << std::endl;
+			std::cout << "\t\t - Print" << std::endl;
+			std::cout << "\t\t\t" << sv << std::endl;
+		}
+
+		std::cout << std::endl << std::endl;
+
+		{
+			std::cout << "\t" << "+ std::string s = ""String View Test_2"";" << std::endl;
+			std::cout << "\t" << "+ std::string_view sv = s;" << std::endl << std::endl;
+			std::string s = "String View Test_2";
+			std::string_view sv = s;
+
+			std::cout << "\t\t - Print" << std::endl;
+			std::cout << "\t\t\t" << sv << std::endl;
 		}
 
 		std::cout << std::endl << std::endl;
