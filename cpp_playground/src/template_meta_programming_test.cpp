3aaf8f7b67f7fb2a49b6c7712a04915114f2a95a
r2road <studior2road@gmail.com>
2021-04-02 12:01:16 +0900

    ...

--- cpp_playground/src/template_meta_programming_test.cpp
+++ cpp_playground/src/template_meta_programming_test.cpp
@@ -190,15 +190,15 @@ namespace template_meta_programming_test
 		{
 			std::cout << "== TMP : Units 2 Character ==" << std::endl;
 
-			std::cout << "\t" << "+ << ConvertUnits2Character<1>::c" << std::endl;
+			std::cout << "\t" << "+ ConvertUnits2Character<1>::c" << std::endl;
 			std::cout << "\t\t" << "result : " << ConvertUnits2Character<1>::c << std::endl;
 
 
-			std::cout << "\t" << "+ << ConvertUnits2Character<9>::c" << std::endl;
+			std::cout << "\t" << "+ ConvertUnits2Character<9>::c" << std::endl;
 			std::cout << "\t\t" << "result : " << ConvertUnits2Character<9>::c << std::endl;
 
 
-			std::cout << "\t" << "+ << ConvertUnits2Character<10>::c" << std::endl;
+			std::cout << "\t" << "+ ConvertUnits2Character<10>::c" << std::endl;
 			std::cout << "\t\t" << "result : " << ConvertUnits2Character<10>::c << std::endl;
 		}
 
