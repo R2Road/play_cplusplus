a1a3b630abe5206255be63a8803fb604ac89f60c
r2road <studior2road@gmail.com>
2021-04-03 00:02:39 +0900

    extract class

--- cpp_playground/src/main.cpp
+++ cpp_playground/src/main.cpp
@@ -1,143 +1,14 @@
-﻿#include <conio.h>
-#include <iostream>
-#include <sstream>
-#include <Windows.h>
+﻿#include <Windows.h>
 
-#include "filesystem_test.h"
-#include "random_test.h"
-#include "shared_pointer_test.h"
-#include "variadic_template_test.h"
-#include "template_meta_programming_test.h"
-#include "optional_test.h"
-#include "variant_test.h"
-#include "tuple_test.h"
-#include "structured_binding_test.h"
-#include "const_pointer_test.h"
-#include "assert_test.h"
-#include "string_view_test.h"
-
-
-std::string MakeMenuString()
-{
-	std::stringstream ss;
-	ss << "+ Menu" << std::endl;
-	ss << "1 : " << filesystem_test::CurrentDirectory::GetTitle() << std::endl;
-	ss << "2 : " << random_test::StatusSaveAndLoad::GetTitle() << std::endl;
-	ss << "3 : " << shared_pointer_test::SharedPtr::GetTitle() << std::endl;
-	ss << "4 : " << variadic_template_test::PrintValues::GetTitle() << std::endl;
-	ss << "5 : " << variadic_template_test::SumValues::GetTitle() << std::endl;
-	ss << "6 : " << variadic_template_test::SubtractValues::GetTitle() << std::endl;
-	ss << "7 : " << template_meta_programming_test::CalculateFactorial::GetTitle() << std::endl;
-	ss << "8 : " << template_meta_programming_test::Integer2String::GetTitle() << std::endl;
-	ss << "9 : " << template_meta_programming_test::CalculatePlaceValue::GetTitle() << std::endl;
-	ss << "Q : " << template_meta_programming_test::Integer2String_II::GetTitle() << std::endl;
-	ss << "W : " << optional_test::Basic::GetTitle() << std::endl;
-	ss << "E : " << variant_test::Basic::GetTitle() << std::endl;
-	ss << "R : " << variadic_template_test::SizeOfArgs::GetTitle() << std::endl;
-	ss << "T : " << tuple_test::Basic::GetTitle() << std::endl;
-	ss << "Y : " << structured_binding_test::Basic::GetTitle() << std::endl;
-	ss << "U : " << random_test::Basic::GetTitle() << std::endl;
-	ss << "I : " << const_pointer_test::Basic::GetTitle() << std::endl;
-	ss << "O : " << assert_test::Basic::GetTitle() << std::endl;
-	ss << "P : " << template_meta_programming_test::MultiTypePackage::GetTitle() << std::endl;
-	ss << "A : " << stringview_test::Basic::GetTitle() << std::endl;
-
-	ss << std::endl << "Press Number" << std::endl;
-
-	return std::string( ss.str() );
-}
-
-void ShowMenu()
-{
-	static std::string menu_string( std::move( MakeMenuString() ) );
-	std::cout << menu_string;
-}
+#include "r2_Scene.h"
 
 int main()
 {
 	SetConsoleTitle( TEXT( "cpp_playground" ) );
 	system( "mode con lines=60 cols=120" );
 
-	int input = 0;
-	while( true )
-	{
-		ShowMenu();
-
-		input = _getch();
-		system( "cls" );
-
-		switch( input )
-		{
-		case '1':
-			filesystem_test::CurrentDirectory::Do();
-			break;
-		case '2':
-			random_test::StatusSaveAndLoad::Do();
-			break;
-		case '3':
-			shared_pointer_test::SharedPtr::Do();
-			break;
-		case '4':
-			variadic_template_test::PrintValues::Do();
-			break;
-		case '5':
-			variadic_template_test::SumValues::Do();
-			break;
-		case '6':
-			variadic_template_test::SubtractValues::Do();
-			break;
-		case '7':
-			template_meta_programming_test::CalculateFactorial::Do();
-			break;
-		case '8':
-			template_meta_programming_test::Integer2String::Do();
-			break;
-		case '9':
-			template_meta_programming_test::CalculatePlaceValue::Do();
-			break;
-		case 'q':
-			template_meta_programming_test::Integer2String_II::Do();
-			break;
-		case 'w':
-			optional_test::Basic::Do();
-			break;
-		case 'e':
-			variant_test::Basic::Do();
-			break;
-		case 'r':
-			variadic_template_test::SizeOfArgs::Do();
-			break;
-		case 't':
-			tuple_test::Basic::Do();
-			break;
-		case 'y':
-			structured_binding_test::Basic::Do();
-			break;
-		case 'u':
-			random_test::Basic::Do();
-			break;
-		case 'i':
-			const_pointer_test::Basic::Do();
-			break;
-		case 'o':
-			assert_test::Basic::Do();
-			break;
-		case 'p':
-			template_meta_programming_test::MultiTypePackage::Do();
-			break;
-
-		case 'a':
-			stringview_test::Basic::Do();
-			break;
-
-		case 27: // ESC
-			return 0;
-		}
-
-		std::cout << std::endl << "Press Any Key" << std::endl;
-		_getch();
-		system( "cls" );
-	}
+	r2::Scene scene;
+	scene.Do();
 
 	return 0;
 }
