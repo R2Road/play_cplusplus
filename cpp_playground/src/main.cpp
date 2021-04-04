cc90c8c46345c04a6eac5223eece74a879c40d9f
r2road <studior2road@gmail.com>
2021-04-04 22:42:03 +0900

    ...

--- cpp_playground/src/main.cpp
+++ cpp_playground/src/main.cpp
@@ -5,12 +5,21 @@
 
 int main()
 {
+	//
+	// Environment
+	//
 	SetConsoleTitle( TEXT( "cpp_playground" ) );
 	system( "mode con lines=60 cols=120" );
 
+	//
+	// Setup
+	//
 	r2::Director director;
 	director.Setup( r2::Scene::Create( &director ) );
 
+	//
+	// Process
+	//
 	director.Update();
 
 	return 0;
