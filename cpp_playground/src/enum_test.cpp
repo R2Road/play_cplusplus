e7a02f3ef3e849a9beb31d43423e535cd657cd55
r2road <studior2road@gmail.com>
2021-04-06 22:13:51 +0900

    update : enum_test - Basic

--- cpp_playground/src/enum_test.cpp
+++ cpp_playground/src/enum_test.cpp
@@ -1,7 +1,8 @@
 #include "enum_test.h"
 
 #include <iostream>
-#include <typeinfo>
+#include <typeinfo> // typeid
+#include <type_traits>
 
 namespace enum_test
 {
@@ -19,11 +20,27 @@ namespace enum_test
 			std::cout << "== Enum ==" << std::endl << std::endl;
 
 			{
-				std::cout << "\t + " << "enum class eTestEnum_1 {};" << std::endl;
-				enum class eTestEnum_1 {};
+				std::cout << "\t + " << "enum eTestOldEnum {};" << std::endl << std::endl;
+				enum eTestOldEnum {};
 
-				std::cout << "\t + " << "typeid( std::underlying_type<eTestEnum_1>::type ).name()" << std::endl;
-				std::cout << "\t\t - " << typeid( std::underlying_type<eTestEnum_1>::type ).name() << std::endl;
+				std::cout << "\t\t - " << "std::is_enum<eTestOldEnum>::value" << std::endl;
+				std::cout << "\t\t\t - " << std::is_enum<eTestOldEnum>::value << std::endl;
+
+				std::cout << "\t\t - " << "typeid( std::underlying_type<eTestEnum_1>::type ).name()" << std::endl;
+				std::cout << "\t\t\t - " << typeid( std::underlying_type<eTestOldEnum>::type ).name() << std::endl;
+			}
+
+			std::cout << std::endl << std::endl;
+
+			{
+				std::cout << "\t + " << "enum class eTestNewEnum {};" << std::endl << std::endl;
+				enum class eTestNewEnum {};
+
+				std::cout << "\t\t - " << "std::is_enum<eTestNewEnum>::value" << std::endl;
+				std::cout << "\t\t\t - " << std::is_enum<eTestNewEnum>::value << std::endl;
+
+				std::cout << "\t\t - " << "typeid( std::underlying_type<eTestNewEnum>::type ).name()" << std::endl;
+				std::cout << "\t\t\t - " << typeid( std::underlying_type<eTestNewEnum>::type ).name() << std::endl;
 			}
 
 			return r2::eTestResult::RunTest;
