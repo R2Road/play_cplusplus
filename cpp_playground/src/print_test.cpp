3498852bd065214c700b8ad2be7f7950b9dffd95
r2road <studior2road@gmail.com>
2021-04-26 02:34:53 +0900

    update : print_test - Basic

--- cpp_playground/src/print_test.cpp
+++ cpp_playground/src/print_test.cpp
@@ -23,6 +23,8 @@ namespace print_test
 			std::cout << r2::split;
 
 			{
+				std::cout << "\t" << "+ Show Number : Use std::cout" << r2::linefeed << r2::linefeed;
+
 				int i = 0xde81;
 
 				std::cout << "\t" << "int i = 0xde81;" << r2::linefeed;
@@ -30,6 +32,20 @@ namespace print_test
 				std::cout << "\t\t" << "Print( 8 : oct ) : " << std::oct << i << r2::linefeed;
 				std::cout << "\t\t" << "Print( 10 : dec ) : " << std::dec << i << r2::linefeed;
 				std::cout << "\t\t" << "Print( 16 : hex ) : " << std::hex << i << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << "\t" << "+ Show Number : Use printf" << r2::linefeed << r2::linefeed;
+
+				int i = 0xde81;
+
+				std::cout << "\t" << "int i = 0xde81;" << r2::linefeed;
+
+				printf( "\t\tPrint( 8 : oct ) : %o\n", i );
+				printf( "\t\tPrint( 10 : dec ) : %d\n", i );
+				printf( "\t\tPrint( 16 : hex ) : %x\n", i );
 			}
 
 			std::cout << r2::split;
