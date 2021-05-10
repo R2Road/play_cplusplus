f25699f077017a8adea980914f29a52a8fed0253
r2road <studior2road@gmail.com>
2021-05-10 22:47:44 +0900

    ...

--- cpp_playground/src/filesystem_test.cpp
+++ cpp_playground/src/filesystem_test.cpp
@@ -158,27 +158,7 @@ namespace filesystem_test
 
 			{
 				std::cout << "\t" << "+ Directory : " << r2::linefeed;
-				std::cout << "\t\t" << p << r2::linefeed;
-				std::cout << r2::linefeed;
-			}
-
-			{
-				std::cout << "\t" << "+ Append with Operator /" << r2::linefeed;
-
-				p /= "test.txt";
-				std::cout << "\t\t" << "p /= \"test.txt\"" << r2::linefeed;
-
-				std::cout << "\t\t\t" << p << r2::linefeed;
-				std::cout << r2::linefeed;
-			}
-
-			{
-				std::cout << "\t" << "+ Remove File Name" << r2::linefeed;
-
-				p.remove_filename();
-				std::cout << "\t\t" << "p.remove_filename();" << r2::linefeed;
-
-				std::cout << "\t\t\t" << p << r2::linefeed;
+				std::cout << "\t\t" << p << r2::linefeed << r2::linefeed;
 				std::cout << r2::linefeed;
 			}
 
@@ -188,18 +168,39 @@ namespace filesystem_test
 				std::cout << "\t" << "+ Append with Operator /" << r2::linefeed;
 
 				p /= "test.txt";
-				std::cout << "\t\t" << "p /= \"test.txt\"" << r2::linefeed;
+				std::cout << "\t\t" << "p /= \"test.txt\"" << r2::linefeed << r2::linefeed;
 
 				std::cout << "\t\t\t" << p << r2::linefeed;
+				std::cout << r2::linefeed;
+
+
+				std::cout << "\t" << "+ Remove File Name" << r2::linefeed;
+
+				p.remove_filename();
+				std::cout << "\t\t" << "p.remove_filename();" << r2::linefeed << r2::linefeed;
+
+				std::cout << "\t\t\t" << p << r2::linefeed << r2::linefeed;
+				std::cout << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << "\t" << "+ Append with Operator /" << r2::linefeed;
+
+				p /= "test.txt";
+				std::cout << "\t\t" << "p /= \"test.txt\"" << r2::linefeed << r2::linefeed;
+
+				std::cout << "\t\t\t" << p << r2::linefeed << r2::linefeed;
 				std::cout << r2::linefeed;
 
 
 				std::cout << "\t" << "+ Remove File Name And Separator" << r2::linefeed;
 
 				p._Remove_filename_and_separator();
-				std::cout << "\t\t" << "p._Remove_filename_and_separator();" << r2::linefeed;
+				std::cout << "\t\t" << "p._Remove_filename_and_separator();" << r2::linefeed << r2::linefeed;
 
-				std::cout << "\t\t\t" << p << r2::linefeed;
+				std::cout << "\t\t\t" << p << r2::linefeed << r2::linefeed;
 				std::cout << r2::linefeed;
 			}
 
