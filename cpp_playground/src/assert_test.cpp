e345dca196c1af50d93bebb4bc20a81682a5e3b4
r2road <studior2road@gmail.com>
2021-05-16 00:11:50 +0900

    ...

--- cpp_playground/src/assert_test.cpp
+++ cpp_playground/src/assert_test.cpp
@@ -19,16 +19,16 @@ namespace assert_test
 	{
 		return []()->r2::eTestResult
 		{
-			std::cout << "# " << GetInstance().GetTitleFunction()() << " #" << r2::linefeed << r2::linefeed;
+			std::cout << "# " << GetInstance().GetTitleFunction()() << " #" << r2::linefeed;
 
-			std::cout << r2::linefeed;
+			std::cout << r2::split;
 
 			{
 				std::cout << "\t" << "+ assert( 1 > 2 && \"What The Fuck\" );" << r2::linefeed << r2::linefeed;
 				assert( 1 > 2 && "What The Fuck" );
 			}
 
-			std::cout << r2::linefeed << r2::linefeed;
+			std::cout << r2::split;
 
 			return r2::eTestResult::RunTest;
 		};
