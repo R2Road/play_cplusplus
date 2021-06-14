9229b228ab0c72d44bb57269662a2fefa22fa48c
r2road <studior2road@gmail.com>
2021-06-15 00:57:27 +0900

    add : r2 - linefeed3

--- cpp_playground/src/pch.h
+++ cpp_playground/src/pch.h
@@ -21,6 +21,13 @@ namespace r2
 	}
 
 	template<class _Elem, class _Traits>
+	inline std::basic_ostream<_Elem, _Traits>& linefeed3( std::basic_ostream<_Elem, _Traits>& _Ostr )
+	{
+		_Ostr << linefeed << linefeed << linefeed;
+		return ( _Ostr );
+	}
+
+	template<class _Elem, class _Traits>
 	inline std::basic_ostream<_Elem, _Traits>& split( std::basic_ostream<_Elem, _Traits>& _Ostr )
 	{
 		_Ostr << r2::linefeed << "====================================================================" << r2::linefeed << r2::linefeed;
