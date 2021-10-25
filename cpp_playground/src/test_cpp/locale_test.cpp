63aeb8d8be4c1aa025c552587b2790dd3336ade9
r2road <studior2road@gmail.com>
2021-10-25 20:15:39 +0900

    update : locale_test - Basic

--- cpp_playground/src/test_cpp/locale_test.cpp
+++ cpp_playground/src/test_cpp/locale_test.cpp
@@ -49,6 +49,16 @@ namespace locale_test
 
 			std::cout << r2::split;
 
+			{
+				const std::string old_locale = setlocale( LC_ALL, nullptr );
+
+				std::cout << r2::tab << "+ Current Locale" << r2::linefeed2;
+				std::cout << r2::tab2 << "const std::string old_locale = setlocale( LC_ALL, nullptr );" << r2::linefeed;
+				std::cout << r2::tab2 << "- " << old_locale << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
 			return r2::eTestResult::RunTest;
 		};
 	}
