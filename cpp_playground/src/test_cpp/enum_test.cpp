ed0aa2740e7d1f54241b0db493ba9caceba7711c
r2road <studior2road@gmail.com>
2021-11-14 20:52:46 +0900

    apply : r2 - tab

--- cpp_playground/src/test_cpp/enum_test.cpp
+++ cpp_playground/src/test_cpp/enum_test.cpp
@@ -24,37 +24,37 @@ namespace enum_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t + " << "enum eTestOldEnum {};" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab << "+ " << "enum eTestOldEnum {};" << r2::linefeed << r2::linefeed;
 				enum eTestOldEnum {};
 
-				std::cout << "\t\t - " << "std::is_enum<eTestOldEnum>::value" << r2::linefeed;
-				std::cout << "\t\t\t - " << std::is_enum<eTestOldEnum>::value << r2::linefeed;
+				std::cout << r2::tab2 << "- " << "std::is_enum<eTestOldEnum>::value" << r2::linefeed;
+				std::cout << r2::tab3 << "- " << std::is_enum<eTestOldEnum>::value << r2::linefeed;
 
-				std::cout << "\t\t - " << "typeid( std::underlying_type<eTestEnum_1>::type ).name()" << r2::linefeed;
-				std::cout << "\t\t\t - " << typeid( std::underlying_type<eTestOldEnum>::type ).name() << r2::linefeed;
+				std::cout << r2::tab2 << "- " << "typeid( std::underlying_type<eTestEnum_1>::type ).name()" << r2::linefeed;
+				std::cout << r2::tab3 << "- " << typeid( std::underlying_type<eTestOldEnum>::type ).name() << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t + " << "enum class eTestNewEnum {};" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab << "+ " << "enum class eTestNewEnum {};" << r2::linefeed << r2::linefeed;
 				enum class eTestNewEnum {};
 
-				std::cout << "\t\t - " << "std::is_enum<eTestNewEnum>::value" << r2::linefeed;
-				std::cout << "\t\t\t - " << std::is_enum<eTestNewEnum>::value << r2::linefeed;
+				std::cout << r2::tab2 << "- " << "std::is_enum<eTestNewEnum>::value" << r2::linefeed;
+				std::cout << r2::tab3 << "- " << std::is_enum<eTestNewEnum>::value << r2::linefeed;
 
-				std::cout << "\t\t - " << "typeid( std::underlying_type<eTestNewEnum>::type ).name()" << r2::linefeed;
-				std::cout << "\t\t\t - " << typeid( std::underlying_type<eTestNewEnum>::type ).name() << r2::linefeed;
+				std::cout << r2::tab2 << "- " << "typeid( std::underlying_type<eTestNewEnum>::type ).name()" << r2::linefeed;
+				std::cout << r2::tab3 << "- " << typeid( std::underlying_type<eTestNewEnum>::type ).name() << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t + " << "enum class eTestNewEnum : char {};" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab << "+ " << "enum class eTestNewEnum : char {};" << r2::linefeed << r2::linefeed;
 				enum class eTestNewEnum : char {};
 
-				std::cout << "\t\t - " << "typeid( std::underlying_type<eTestNewEnum>::type ).name()" << r2::linefeed;
-				std::cout << "\t\t\t - " << typeid( std::underlying_type<eTestNewEnum>::type ).name() << r2::linefeed;
+				std::cout << r2::tab2 << "- " << "typeid( std::underlying_type<eTestNewEnum>::type ).name()" << r2::linefeed;
+				std::cout << r2::tab3 << "- " << typeid( std::underlying_type<eTestNewEnum>::type ).name() << r2::linefeed;
 			}
 
 			std::cout << r2::split;
@@ -88,21 +88,21 @@ namespace enum_test
 
 			std::cout << r2::split;
 
-			std::cout << "\t" << "template<typename EnumT>" << r2::linefeed;
-			std::cout << "\t" << "constexpr auto Enum2Value( EnumT e )" << r2::linefeed;
-			std::cout << "\t" << "{" << r2::linefeed;
-			std::cout << "\t\t" << "return static_cast<std::underlying_type_t<EnumT>>( e );" << r2::linefeed;
-			std::cout << "\t" << "}" << r2::linefeed;
+			std::cout << r2::tab << "template<typename EnumT>" << r2::linefeed;
+			std::cout << r2::tab << "constexpr auto Enum2Value( EnumT e )" << r2::linefeed;
+			std::cout << r2::tab << "{" << r2::linefeed;
+			std::cout << r2::tab2 << "return static_cast<std::underlying_type_t<EnumT>>( e );" << r2::linefeed;
+			std::cout << r2::tab << "}" << r2::linefeed;
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "enum eOldEnum" << r2::linefeed;
-				std::cout << "\t" << "{" << r2::linefeed;
-				std::cout << "\t\t" << "one" << r2::linefeed;
-				std::cout << "\t\t" << ", two" << r2::linefeed;
-				std::cout << "\t\t" << ", three" << r2::linefeed;
-				std::cout << "\t" << "}" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab << "enum eOldEnum" << r2::linefeed;
+				std::cout << r2::tab << "{" << r2::linefeed;
+				std::cout << r2::tab2 << "one" << r2::linefeed;
+				std::cout << r2::tab2 << ", two" << r2::linefeed;
+				std::cout << r2::tab2 << ", three" << r2::linefeed;
+				std::cout << r2::tab << "}" << r2::linefeed << r2::linefeed;
 
 				enum eOldEnum
 				{
@@ -111,20 +111,20 @@ namespace enum_test
 					, three
 				};
 
-				std::cout << "\t" << "+ " << "Enum2Value( eOldEnum::three )" << r2::linefeed;
-				std::cout << "\t\t" << "- " << Enum2Value( eOldEnum::three ) << r2::linefeed;
-				std::cout << "\t\t" << "- " << typeid( Enum2Value( eOldEnum::three ) ).name() << r2::linefeed;
+				std::cout << r2::tab << "+ " << "Enum2Value( eOldEnum::three )" << r2::linefeed;
+				std::cout << r2::tab2 << "- " << Enum2Value( eOldEnum::three ) << r2::linefeed;
+				std::cout << r2::tab2 << "- " << typeid( Enum2Value( eOldEnum::three ) ).name() << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "enum class eNewEnum : short" << r2::linefeed;
-				std::cout << "\t" << "{" << r2::linefeed;
-				std::cout << "\t\t" << "hana" << r2::linefeed;
-				std::cout << "\t\t" << ", dul" << r2::linefeed;
-				std::cout << "\t\t" << ", set" << r2::linefeed;
-				std::cout << "\t" << "}" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab << "enum class eNewEnum : short" << r2::linefeed;
+				std::cout << r2::tab << "{" << r2::linefeed;
+				std::cout << r2::tab2 << "hana" << r2::linefeed;
+				std::cout << r2::tab2 << ", dul" << r2::linefeed;
+				std::cout << r2::tab2 << ", set" << r2::linefeed;
+				std::cout << r2::tab << "}" << r2::linefeed << r2::linefeed;
 
 				enum class eNewEnum : short
 				{
@@ -133,9 +133,9 @@ namespace enum_test
 					, set
 				};
 
-				std::cout << "\t" << "+ " << "Enum2Value( eNewEnum::dul )" << r2::linefeed;
-				std::cout << "\t\t" << "- " << Enum2Value( eNewEnum::dul ) << r2::linefeed;
-				std::cout << "\t\t" << "- " << typeid( Enum2Value( eNewEnum::dul ) ).name() << r2::linefeed;
+				std::cout << r2::tab << "+ " << "Enum2Value( eNewEnum::dul )" << r2::linefeed;
+				std::cout << r2::tab2 << "- " << Enum2Value( eNewEnum::dul ) << r2::linefeed;
+				std::cout << r2::tab2 << "- " << typeid( Enum2Value( eNewEnum::dul ) ).name() << r2::linefeed;
 			}
 
 			std::cout << r2::split;
