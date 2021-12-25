2674e0049816f84aa9ddebd68b01a7710625e1c9
r2road <studior2road@gmail.com>
2021-12-26 06:39:33 +0900

    update : atomic_test - IsLockFree

--- cpp_playground/src/test_cpp/atomic_test.cpp
+++ cpp_playground/src/test_cpp/atomic_test.cpp
@@ -2,10 +2,28 @@
 #include "atomic_test.h"
 
 #include <atomic>
-#include <string>
+#include <memory>
 #include <type_traits>
 
 #include "base/r2_eTestResult.h"
+
+struct TestStruct1
+{
+	int i;
+};
+
+struct TestStruct2
+{
+	int i;
+	float f;
+};
+
+struct TestStruct3
+{
+	int i;
+	float f;
+	bool b;
+};
 
 namespace atomic_test
 {
@@ -39,7 +57,7 @@ namespace atomic_test
 			std::cout << r2::split;
 
 			{
-				std::cout << r2::tab << "+ Test" << r2::linefeed2;
+				std::cout << r2::tab << "+ Test 1" << r2::linefeed2;
 
 				IsLockFreePrinter<bool>().Print();
 				IsLockFreePrinter<char>().Print();
@@ -49,6 +67,17 @@ namespace atomic_test
 				IsLockFreePrinter<double>().Print();
 				IsLockFreePrinter<int64_t>().Print();
 				IsLockFreePrinter<void*>().Print();
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "+ Test 2" << r2::linefeed2;
+
+				IsLockFreePrinter<TestStruct1>().Print();
+
+				std::cout << r2::tab << "# 멤버가 2개 이상인 struct 의 atomic을 만들면 어쩔때는 static assert가 걸리고 어쩔때는 안걸린다." << r2::linefeed;
+				std::cout << r2::tab << "# vs2017에 버그가 있는듯." << r2::linefeed2;
 			}
 
 			std::cout << r2::split;
