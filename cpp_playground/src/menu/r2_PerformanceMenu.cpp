ce1aac9bfc8993db02df1bcda321739f5ca3b34e
r2road <studior2road@gmail.com>
2022-01-04 15:09:54 +0900

    ...

--- cpp_playground/src/menu/r2_PerformanceMenu.cpp
+++ cpp_playground/src/menu/r2_PerformanceMenu.cpp
@@ -20,10 +20,12 @@ namespace r2
 			ret->AddChild( '3', performance_2_test::LoopWithBuildInArray::GetInstance() );
 			ret->AddChild( '4', performance_2_test::LoopWithSTDArray::GetInstance() );
 
-			ret->AddChild( '5', performance_2_test::BuiltInArrayIteration::GetInstance() );
-			ret->AddChild( '6', performance_2_test::STDArrayIteration::GetInstance() );
-			ret->AddChild( '7', performance_2_test::VectorIteration::GetInstance() );
-			ret->AddChild( '8', performance_2_test::ListIteration::GetInstance() );
+			ret->AddLineFeed();
+
+			ret->AddChild( 'q', performance_2_test::BuiltInArrayIteration::GetInstance() );
+			ret->AddChild( 'w', performance_2_test::STDArrayIteration::GetInstance() );
+			ret->AddChild( 'e', performance_2_test::VectorIteration::GetInstance() );
+			ret->AddChild( 'r', performance_2_test::ListIteration::GetInstance() );
 
 
 			ret->AddSplit();
