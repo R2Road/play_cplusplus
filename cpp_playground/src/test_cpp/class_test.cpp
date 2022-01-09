97a0be8fc47e2bf2c00f6f493dee910e69436e1e
r2road <studior2road@gmail.com>
2022-01-09 20:42:17 +0900

    update : class_test -  MemberAdress

--- cpp_playground/src/test_cpp/class_test.cpp
+++ cpp_playground/src/test_cpp/class_test.cpp
@@ -360,12 +360,24 @@ namespace class_test
 			std::cout << r2::split;
 
 			{
-				class MATestClass1
+				class TestClass1
 				{
 				public:
 					int a;
 					int b;
 				};
+
+				TestClass1 tc1;
+				auto pTc1 = &tc1;
+
+				std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
+				std::cout << r2::tab2 << "TestClass1 tc1;" << r2::linefeed;
+				std::cout << r2::tab2 << "auto pTc1 = &tc1;" << r2::linefeed2;
+
+				std::cout << r2::tab << "+ Body Adress" << r2::linefeed2;
+				std::cout << r2::tab2 << "&tc1 : " << &tc1 << r2::linefeed;
+				std::cout << r2::tab2 << "pTc1 : " << pTc1 << r2::linefeed;
+				std::cout << r2::tab2 << "&( *pTc1 ) : " << &( *pTc1 ) << r2::linefeed2;
 			}
 
 			std::cout << r2::split;
