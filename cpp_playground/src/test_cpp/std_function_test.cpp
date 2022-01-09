3bc1856165484ce1366ed733754315b7d6a889fe
r2road <studior2road@gmail.com>
2022-01-09 17:08:51 +0900

    ...

--- cpp_playground/src/test_cpp/std_function_test.cpp
+++ cpp_playground/src/test_cpp/std_function_test.cpp
@@ -223,14 +223,14 @@ namespace std_function_test
 				std::cout << r2::tab << "+ 람다로 초기화된 std::function 에서 람다의 함수 포인터를 꺼내오는 방법은 모르겠다." << r2::linefeed;
 				std::cout << r2::tab << "+ 꺼내오지 못하니 비교도 할 수 없다." << r2::linefeed2;
 
-				STDFunctionT tf1 = []() {};
-				STDFunctionT tf2 = tf1;
+				STDFunctionT sf1 = []() {};
+				STDFunctionT sf2 = sf1;
 
-				std::cout << r2::tab2 << "STDFunctionT tf1 = []() {};" << r2::linefeed;
-				std::cout << r2::tab2 << "STDFunctionT tf2 = tf1" << r2::linefeed2;
+				std::cout << r2::tab2 << "STDFunctionT sf1 = []() {};" << r2::linefeed;
+				std::cout << r2::tab2 << "STDFunctionT tf2 = sf1" << r2::linefeed2;
 
-				std::cout << r2::tab2 << "nullptr == tf1.target<FunctionPointerT>()" << r2::linefeed;
-				if( nullptr == tf1.target<FunctionPointerT>() )
+				std::cout << r2::tab2 << "nullptr == sf1.target<FunctionPointerT>()" << r2::linefeed;
+				if( nullptr == sf1.target<FunctionPointerT>() )
 				{
 					std::cout << r2::tab3 << "- Is Equal" << r2::linefeed;
 				}
@@ -241,8 +241,8 @@ namespace std_function_test
 
 				std::cout << r2::linefeed;
 
-				std::cout << r2::tab2 << "tf1.target<FunctionPointerT>() == tf1.target<FunctionPointerT>()" << r2::linefeed;
-				if( tf1.target<FunctionPointerT>() == tf1.target<FunctionPointerT>() )
+				std::cout << r2::tab2 << "sf1.target<FunctionPointerT>() == sf1.target<FunctionPointerT>()" << r2::linefeed;
+				if( sf1.target<FunctionPointerT>() == sf1.target<FunctionPointerT>() )
 				{
 					std::cout << r2::tab3 << "- Is Equal" << r2::linefeed;
 				}
@@ -253,8 +253,8 @@ namespace std_function_test
 
 				std::cout << r2::linefeed;
 
-				std::cout << r2::tab2 << "tf1.target<FunctionPointerT>() == tf2.target<FunctionPointerT>()" << r2::linefeed;
-				if( tf1.target<FunctionPointerT>() == tf2.target<FunctionPointerT>() )
+				std::cout << r2::tab2 << "sf1.target<FunctionPointerT>() == sf1.target<FunctionPointerT>()" << r2::linefeed;
+				if( sf1.target<FunctionPointerT>() == sf2.target<FunctionPointerT>() )
 				{
 					std::cout << r2::tab3 << "- Is Equal" << r2::linefeed;
 				}
