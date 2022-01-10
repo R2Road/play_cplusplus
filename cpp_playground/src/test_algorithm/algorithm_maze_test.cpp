2ebab7076c4f9536de7d79918a3a31772d250421
r2road <studior2road@gmail.com>
2022-01-10 11:58:23 +0900

    update : algorithm_maze_test - Basic

--- cpp_playground/src/test_algorithm/algorithm_maze_test.cpp
+++ cpp_playground/src/test_algorithm/algorithm_maze_test.cpp
@@ -3,12 +3,18 @@
 
 #include "base/r2_eTestResult.h"
 
+#include "r2/r2_Direction4.h"
 #include "r2/r2_Grid.h"
+#include "r2/r2_Point.h"
 
 namespace
 {
 	void ShowGrid( const r2::Grid<int> grid )
 	{
+		system( "cls" );
+
+		std::cout << "# " << algorithm_maze_test::Basic::GetInstance().GetTitleFunction()( ) << " #" << r2::linefeed;
+
 		int x = 0;
 		for( const auto i : grid )
 		{
@@ -22,6 +28,8 @@ namespace
 
 			++x;
 		}
+
+		std::cout << r2::split;
 	}
 }
 
@@ -42,13 +50,42 @@ namespace algorithm_maze_test
 
 			std::cout << r2::split;
 
+			struct Cell
+			{
+				r2::Direction4 direction;
+			};
+
+
+			r2::Direction4 current_direction;
+			r2::Point current_point{ 0, 0 };
+			r2::Point temp_point{ 0, 0 };
 			r2::Grid<int> grid;
 			grid.Reset( 19, 19 );
 
-			std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
-
 			{
-				ShowGrid( grid );
+				grid.Set( current_point.x, current_point.y, true );
+
+				int stay_count = 0;
+				while( 4 >= stay_count )
+				{
+					temp_point.x = current_point.x + current_direction.GetPoint().x;
+					temp_point.y = current_point.y + current_direction.GetPoint().y;
+
+					if( !grid.IsIn( temp_point.x, temp_point.y ) ||  0 < grid.Get( temp_point.x, temp_point.y ) )
+					{
+						current_direction.Rotate( true );
+						++stay_count;
+
+						continue;
+					}
+
+					grid.Set( temp_point.x, temp_point.y, 1 );
+
+					current_point = temp_point;
+					stay_count = 0;
+
+					ShowGrid( grid );
+				}
 			}
 
 			std::cout << r2::split;
