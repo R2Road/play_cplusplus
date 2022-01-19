a1ce58144e9b7d135a411c74e108e00680b1c428
r2road <studior2road@gmail.com>
2022-01-20 00:11:49 +0900

    update : test - console_input_test - EnableWindowInput

--- cpp_playground/src/test_cpp/console_input_test.cpp
+++ cpp_playground/src/test_cpp/console_input_test.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "console_input_test.h"
 
-#include <conio.h>
+#include <assert.h>
+#include <conio.h> // _kbhit(), _getch()
+#include <windows.h>
 
 #include "base/r2_eTestResult.h"
 
@@ -44,7 +46,13 @@ namespace console_input_test
 	}
 
 
+	void ShowCurrentConsoleMode()
+	{
+		DWORD temp_console_mode;
 
+		GetConsoleMode( GetStdHandle( STD_INPUT_HANDLE ), &temp_console_mode );
+		std::cout << r2::tab << "Current Console Mode : " << temp_console_mode << r2::linefeed;
+	}
 	r2::iTest::TitleFunc EnableWindowInput::GetTitleFunction() const
 	{
 		return []()->const char*
@@ -60,7 +68,60 @@ namespace console_input_test
 
 			std::cout << r2::split;
 
-			{}
+			HANDLE hStdInputHandle = GetStdHandle( STD_INPUT_HANDLE );
+			DWORD last_console_mode;
+
+			std::cout << r2::tab << "+ Declaration" << r2::linefeed2;
+			std::cout << r2::tab2 << "HANDLE hStdInputHandle = GetStdHandle( STD_INPUT_HANDLE );" << r2::linefeed;
+			std::cout << r2::tab2 << "DWORD last_console_mode;" << r2::linefeed;
+
+			std::cout << r2::split;
+
+			{
+				if( !GetConsoleMode( hStdInputHandle, &last_console_mode ) )
+				{
+					assert( false && "GetConsoleMode( hStdInputHandle, &last_console_mode )" );
+				}
+
+				std::cout << r2::tab << "+ Backup Console Mode" << r2::linefeed2;
+				std::cout << r2::tab2 << "GetConsoleMode( hStdInputHandle, &last_console_mode )" << r2::linefeed2;
+				
+				std::cout << r2::linefeed;
+				ShowCurrentConsoleMode();
+			}
+
+			std::cout << r2::split;
+
+			{
+				const DWORD new_console_mode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+				if( !SetConsoleMode( hStdInputHandle, new_console_mode ) )
+				{
+					assert( false && "SetConsoleMode( hStdInputHandle, new_console_mode )" );
+				}
+
+				std::cout << r2::tab << "+ Change Console Mode" << r2::linefeed2;
+				std::cout << r2::tab2 << "const DWORD new_console_mode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;" << r2::linefeed;
+				std::cout << r2::tab2 << "SetConsoleMode( hStdInputHandle, new_console_mode )" << r2::linefeed;
+				std::cout << r2::tab3 << "> " << new_console_mode << r2::linefeed;
+
+				std::cout << r2::linefeed;
+				ShowCurrentConsoleMode();
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "Press Any Key : Rollback" << r2::linefeed;
+				_getch();
+
+				//
+				// Rollback
+				//
+				SetConsoleMode( hStdInputHandle, last_console_mode );
+				
+				std::cout << r2::linefeed;
+				ShowCurrentConsoleMode();
+			}
 
 			std::cout << r2::split;
 
