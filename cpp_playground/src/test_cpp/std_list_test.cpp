47517f05c43126076a30847e906c127578b48d18
r2road <studior2road@gmail.com>
2022-01-24 01:12:32 +0900

    update : test - std_list_test - Splice

--- cpp_playground/src/test_cpp/std_list_test.cpp
+++ cpp_playground/src/test_cpp/std_list_test.cpp
@@ -46,6 +46,85 @@ namespace std_list_test
 
 			std::cout << r2::split;
 
+			{
+				auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 3 );
+				test_container_2.splice( test_container_2.begin(), test_container_1, target_itr );
+
+				std::cout << r2::tab << "+ Process" << r2::linefeed;
+				std::cout << r2::tab2 << "auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 3 );" << r2::linefeed;
+				std::cout << r2::tab2 << "test_container_2.splice( test_container_2.begin(), test_container_1, target_itr );" << r2::linefeed2;
+
+				std::cout << r2::tab << "+ Result" << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_1 : ";
+				for( const auto i : test_container_1 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_2 : ";
+				for( const auto i : test_container_2 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 4 );
+				test_container_2.splice( test_container_2.begin(), test_container_1, target_itr, test_container_1.end() );
+
+				std::cout << r2::tab << "+ Process" << r2::linefeed;
+				std::cout << r2::tab2 << "auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 4 );" << r2::linefeed;
+				std::cout << r2::tab2 << "test_container_2.splice( test_container_2.begin(), test_container_1, target_itr, test_container_1.end() );" << r2::linefeed2;
+
+				std::cout << r2::tab << "+ Result" << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_1 : ";
+				for( const auto i : test_container_1 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_2 : ";
+				for( const auto i : test_container_2 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 1 );
+				auto end_itr = target_itr;
+				++end_itr;
+				test_container_2.splice( test_container_2.begin(), test_container_1, target_itr, end_itr );
+
+				std::cout << r2::tab << "+ Process" << r2::linefeed;
+				std::cout << r2::tab2 << "auto target_itr = std::find( test_container_1.begin(), test_container_1.end(), 1 );" << r2::linefeed;
+				std::cout << r2::tab2 << "auto end_itr = target_itr;" << r2::linefeed;
+				std::cout << r2::tab2 << "++end_itr;" << r2::linefeed;
+				std::cout << r2::tab2 << "test_container_2.splice( test_container_2.begin(), test_container_1, target_itr, end_itr );" << r2::linefeed2;
+
+				std::cout << r2::tab << "+ Result" << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_1 : ";
+				for( const auto i : test_container_1 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+				std::cout << r2::tab2 << "- test_container_2 : ";
+				for( const auto i : test_container_2 )
+				{
+					std::cout << i << " ";
+				}
+				std::cout << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
 			return r2::eTestResult::RunTest;
 		};
 	}
