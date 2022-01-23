243fdb671cd615bcff6f214cd046a75adcaef8c6
r2road <studior2road@gmail.com>
2022-01-23 21:47:51 +0900

    add class : r2algorithm - AStarPathBuilder

---
+++ cpp_playground/src/algorithm/r2algorithm_AStarPathBuilder.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "r2algorithm_AStarPathBuilder.h"
+
+namespace r2algorithm
+{
+	AStarPathBuilder::AStarPathBuilder()
+	{}
+}
---
+++ cpp_playground/src/algorithm/r2algorithm_AStarPathBuilder.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace r2algorithm
+{
+	class AStarPathBuilder
+	{
+	public:
+		AStarPathBuilder();
+	};
+}
