f76f26e7de0552fd13de1f2d4a6ac94e341fa904
r2road <studior2road@gmail.com>
2022-01-25 20:02:57 +0900

    ...

--- cpp_playground/src/test_cpp/optional_test.cpp
+++ cpp_playground/src/test_cpp/optional_test.cpp
@@ -47,65 +47,65 @@ namespace optional_test
 
 			std::cout << r2::split;
 
-			std::cout << "\t" << "+ Make Test Struct" << r2::linefeed;
-			std::cout << "\t\t" << "- TestStruct ts;" << r2::linefeed;
-			std::cout << "\t\t\t";
+			std::cout << r2::tab << "+ Make Test Struct" << r2::linefeed;
+			std::cout << r2::tab2 << "- TestStruct ts;" << r2::linefeed;
+			std::cout << r2::tab3;
 			TestStruct ts;
 
 
 			std::cout << r2::linefeed << r2::linefeed;
 
 
-			std::cout << "\t" << "+ Make Optional" << r2::linefeed;
-			std::cout << "\t\t" << "- std::optional<TestStruct> op_ts;" << r2::linefeed;
+			std::cout << r2::tab << "+ Make Optional" << r2::linefeed;
+			std::cout << r2::tab2 << "- std::optional<TestStruct> op_ts;" << r2::linefeed;
 			std::optional<TestStruct> op_ts;
-			std::cout << "\t\t" << "- Call : has_value()" << r2::linefeed;
-			std::cout << "\t\t\t" << op_ts.has_value() << r2::linefeed;
-			//std::cout << "\t\t\t" << op_ts.value().i << r2::linefeed; // throw exception : op_ts is nullopt
+			std::cout << r2::tab2 << "- Call : has_value()" << r2::linefeed;
+			std::cout << r2::tab3 << op_ts.has_value() << r2::linefeed;
+			//std::cout << r2::tab3 << op_ts.value().i << r2::linefeed; // throw exception : op_ts is nullopt
 
 
 			std::cout << r2::linefeed << r2::linefeed;
 
 
-			std::cout << "\t" << "+ Assign 2 Optional " << r2::linefeed;
-			std::cout << "\t\t" << "- op_ts = ts" << r2::linefeed;
-			std::cout << "\t\t\t";
+			std::cout << r2::tab << "+ Assign 2 Optional " << r2::linefeed;
+			std::cout << r2::tab2 << "- op_ts = ts" << r2::linefeed;
+			std::cout << r2::tab3;
 			op_ts = ts;
-			std::cout << "\t\t" << "- Call : has_value()" << r2::linefeed;
-			std::cout << "\t\t\t" << op_ts.has_value() << r2::linefeed;
+			std::cout << r2::tab2 << "- Call : has_value()" << r2::linefeed;
+			std::cout << r2::tab3 << op_ts.has_value() << r2::linefeed;
 
 
 			std::cout << r2::linefeed << r2::linefeed;
 
 
-			std::cout << "\t" << "+ Value() " << r2::linefeed;
-			std::cout << "\t\t" << "- op_ts.value().I;" << r2::linefeed;
-			std::cout << "\t\t\t" << op_ts.value().I << r2::linefeed;
+			std::cout << r2::tab << "+ Value() " << r2::linefeed;
+			std::cout << r2::tab2 << "- op_ts.value().I;" << r2::linefeed;
+			std::cout << r2::tab3 << op_ts.value().I << r2::linefeed;
 
 
 			std::cout << r2::linefeed << r2::linefeed;
 
 
-			std::cout << "\t" << "+ GetTestStruct() " << r2::linefeed;
+			std::cout << r2::tab << "+ GetTestStruct() " << r2::linefeed;
 			{
-				std::cout << "\t\t" << "- GetTestStruct( 0 );" << r2::linefeed;
-				std::cout << "\t\t\t";
+				std::cout << r2::tab2 << "- GetTestStruct( 0 );" << r2::linefeed;
+				std::cout << r2::tab3;
 				const auto result = GetTestStruct( 0 );
-				std::cout << "\t\t" << "- Call : has_value()" << r2::linefeed;
-				std::cout << "\t\t\t" << result.has_value() << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Call : has_value()" << r2::linefeed;
+				std::cout << r2::tab3 << result.has_value() << r2::linefeed << r2::linefeed;
 			}
 			{
-				std::cout << "\t\t" << "- GetTestStruct( 1 );" << r2::linefeed;
-				std::cout << "\t\t\t";
+				std::cout << r2::tab2 << "- GetTestStruct( 1 );" << r2::linefeed;
+				std::cout << r2::tab3;
 				const auto result = GetTestStruct( 1 );
-				std::cout << "\t\t" << "- Call : has_value()" << r2::linefeed;
-				std::cout << "\t\t\t" << result.has_value() << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Call : has_value()" << r2::linefeed;
+				std::cout << r2::tab3 << result.has_value() << r2::linefeed << r2::linefeed;
 			}
 			{
-				std::cout << "\t\t" << "- GetTestStruct( 2 );" << r2::linefeed;
+				std::cout << r2::tab2 << "- GetTestStruct( 2 );" << r2::linefeed;
 				const auto result = GetTestStruct( 2 );
-				std::cout << "\t\t" << "- Call : has_value()" << r2::linefeed;
-				std::cout << "\t\t\t" << result.has_value() << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Call : has_value()" << r2::linefeed;
+				std::cout << r2::tab3 << result.has_value() << r2::linefeed << r2::linefeed;
 			}
 
 
@@ -113,28 +113,28 @@ namespace optional_test
 
 
 			{
-				std::cout << "\t" << "+ Reference " << r2::linefeed;
+				std::cout << r2::tab << "+ Reference " << r2::linefeed;
 
-				std::cout << "\t\t" << "- TestStruct t_1;" << r2::linefeed;
-				std::cout << "\t\t\t";
+				std::cout << r2::tab2 << "- TestStruct t_1;" << r2::linefeed;
+				std::cout << r2::tab3;
 				TestStruct t_1;
 				std::cout << r2::linefeed;
 
-				std::cout << "\t\t" << "- TestStruct t_2;" << r2::linefeed;
-				std::cout << "\t\t\t";
+				std::cout << r2::tab2 << "- TestStruct t_2;" << r2::linefeed;
+				std::cout << r2::tab3;
 				TestStruct t_2;
 				std::cout << r2::linefeed;
 
 				//
 				//std::optional<TestStruct&> maybe_test_struct_ref = t_1;
 				//
-				std::cout << "\t\t" << "- Not Working : std::optional<TestStruct&> maybe_test_struct_ref = t_1;" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Not Working : std::optional<TestStruct&> maybe_test_struct_ref = t_1;" << r2::linefeed << r2::linefeed;
 
 				std::optional<std::reference_wrapper<TestStruct>> maybe_test_struct_ref = t_1;
-				std::cout << "\t\t" << "- Working : std::optional<std::reference_wrapper<TestStruct>> maybe_test_struct_ref = t_1;" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Working : std::optional<std::reference_wrapper<TestStruct>> maybe_test_struct_ref = t_1;" << r2::linefeed << r2::linefeed;
 
 				maybe_test_struct_ref = t_2;
-				std::cout << "\t\t" << "- Working : maybe_test_struct_ref = t_2;" << r2::linefeed << r2::linefeed;
+				std::cout << r2::tab2 << "- Working : maybe_test_struct_ref = t_2;" << r2::linefeed << r2::linefeed;
 			}
 
 			std::cout << r2::split;
