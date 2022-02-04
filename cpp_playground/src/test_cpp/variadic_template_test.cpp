8dffc07f6603fbb9e0b3cf95db73d917deefb4a1
r2road <studior2road@gmail.com>
2022-02-04 12:34:15 +0900

    ...

--- cpp_playground/src/test_cpp/variadic_template_test.cpp
+++ cpp_playground/src/test_cpp/variadic_template_test.cpp
@@ -29,8 +29,8 @@ namespace variadic_template_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ ArgsCount<int, float, int, int, bool>::size" << r2::linefeed;
-				std::cout << "\t\t" << ArgsCount<int, float, int, int, bool>::size << r2::linefeed;
+				std::cout << r2::tab << "+ ArgsCount<int, float, int, int, bool>::size" << r2::linefeed;
+				std::cout << r2::tab2 << ArgsCount<int, float, int, int, bool>::size << r2::linefeed;
 			}
 
 			std::cout << r2::split;
@@ -47,18 +47,18 @@ namespace
 {
 	template <typename T>
 	void print( T arg ) {
-		std::cout << "\t\t" << "call - print( T arg ) : " << arg;
+		std::cout << r2::tab2 << "call - print( T arg ) : " << arg;
 	}
 
 	template <typename T, typename... Types>
 	void print( T arg, Types... args ) {
-		std::cout << "\t\t" << "call - print( T arg, Types... args ) : " << arg << r2::linefeed;
+		std::cout << r2::tab2 << "call - print( T arg, Types... args ) : " << arg << r2::linefeed;
 		print( args... );
 	}
 
 	template <typename... Types>
 	void print_args_1( Types... args ) {
-		std::cout << "\t\t" << "call - print_args_1( Types... args )" << r2::linefeed;
+		std::cout << r2::tab2 << "call - print_args_1( Types... args )" << r2::linefeed;
 		print( args... );
 		std::cout << r2::linefeed;
 	}
@@ -66,8 +66,8 @@ namespace
 
 	template <typename... Types>
 	void print_args_2( Types... args ) {
-		std::cout << "\t\t" << "call - print_args_2( Types... args )" << r2::linefeed;
-		std::cout << "\t\t\t - ";
+		std::cout << r2::tab2 << "call - print_args_2( Types... args )" << r2::linefeed;
+		std::cout << r2::tab3 << "- ";
 		( std::cout << ... << args ) << r2::linefeed;
 	}
 }
@@ -89,7 +89,7 @@ namespace variadic_template_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ print_args_1( 1, 2, 3, 4, \"variadic_template_test\", 3.141592 )" << r2::linefeed;
+				std::cout << r2::tab << "+ print_args_1( 1, 2, 3, 4, \"variadic_template_test\", 3.141592 )" << r2::linefeed;
 
 				print_args_1( 1, 2, 3, 4, "variadic_template_test", 3.141592 );
 			}
@@ -97,7 +97,7 @@ namespace variadic_template_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ print_args_2( 1, 2, 3, 4, \"variadic_template_test\", 3.141592 )" << r2::linefeed;
+				std::cout << r2::tab << "+ print_args_2( 1, 2, 3, 4, \"variadic_template_test\", 3.141592 )" << r2::linefeed;
 
 				print_args_2( 1, 2, 3, 4, "variadic_template_test", 3.141592 );
 			}
@@ -158,43 +158,43 @@ namespace variadic_template_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ sum_args_with_fold_1( 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( ... + integer_list )" << r2::linefeed;
+				std::cout << r2::tab << "+ sum_args_with_fold_1( 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( ... + integer_list )" << r2::linefeed;
 
 				const auto sum_result = sum_args_with_fold_1( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
 
-				std::cout << "\t\t\t" << sum_result << r2::linefeed;
+				std::cout << r2::tab3 << sum_result << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ sum_args_with_fold_2( 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( integer_list + ... )" << r2::linefeed;
+				std::cout << r2::tab << "+ sum_args_with_fold_2( 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( integer_list + ... )" << r2::linefeed;
 
 				const auto sum_result = sum_args_with_fold_2( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
 
-				std::cout << "\t\t\t" << sum_result << r2::linefeed;
+				std::cout << r2::tab3 << sum_result << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ SumArgs<1>::result" << r2::linefeed;
+				std::cout << r2::tab << "+ SumArgs<1>::result" << r2::linefeed;
 
 				const auto sum_result = SumArgs<1>::result;
 
-				std::cout << "\t\t\t" << sum_result << r2::linefeed;
+				std::cout << r2::tab3 << sum_result << r2::linefeed;
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ SumArgs<1, 2, 3, 4, 5, 6, 7, 8, 9>::result" << r2::linefeed;
+				std::cout << r2::tab << "+ SumArgs<1, 2, 3, 4, 5, 6, 7, 8, 9>::result" << r2::linefeed;
 
 				const auto sum_result = SumArgs<1, 2, 3, 4, 5, 6, 7, 8, 9>::result;
 
-				std::cout << "\t\t\t" << sum_result << r2::linefeed;
+				std::cout << r2::tab3 << sum_result << r2::linefeed;
 			}
 
 			std::cout << r2::split;
@@ -247,85 +247,85 @@ namespace variadic_template_test
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ int subtract_args_with_fold_1(IntegerList... integer_list )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( integer_list - ... );" << r2::linefeed;
+				std::cout << r2::tab << "+ int subtract_args_with_fold_1(IntegerList... integer_list )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( integer_list - ... );" << r2::linefeed;
 				std::cout << r2::linefeed;
 
 				{
-					std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 10, 5 );" << r2::linefeed;
+					std::cout << r2::tab2 << "Call : subtract_args_with_fold_1( 10, 5 );" << r2::linefeed;
 
-					std::cout << "\t\t\t" << subtract_args_with_fold_1( 10, 5 ) << r2::linefeed;
+					std::cout << r2::tab3 << subtract_args_with_fold_1( 10, 5 ) << r2::linefeed;
 				}
 
 				std::cout << r2::linefeed;
 
 				{
-					std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 5, 10 );" << r2::linefeed;
+					std::cout << r2::tab2 << "Call : subtract_args_with_fold_1( 5, 10 );" << r2::linefeed;
 
-					std::cout << "\t\t\t" << subtract_args_with_fold_1( 5, 10 ) << r2::linefeed;
+					std::cout << r2::tab3 << subtract_args_with_fold_1( 5, 10 ) << r2::linefeed;
 				}
 
 				std::cout << r2::linefeed;
 
 				{
-					std::cout << "\t\t" << "Call : subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 );" << r2::linefeed;
+					std::cout << r2::tab2 << "Call : subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 );" << r2::linefeed;
 
-					std::cout << "\t\t\t" << subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
+					std::cout << r2::tab3 << subtract_args_with_fold_1( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
 				}
 
 				std::cout << r2::linefeed;
 
 				{
-					std::cout << "\t\t" << "Test : ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) );" << r2::linefeed;
+					std::cout << r2::tab2 << "Test : ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) );" << r2::linefeed;
 
-					std::cout << "\t\t\t" << ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) ) << r2::linefeed;
-				}
-			}
-
-			std::cout << r2::split;
-
-			{
-				std::cout << "\t" << "+ int subtract_args_with_fold_2(IntegerList... integer_list )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( ... - integer_list );" << r2::linefeed;
-				std::cout << r2::linefeed;
-
-				{
-					std::cout << "\t\t" << "Call : subtract_args_with_fold_2( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
-
-					std::cout << "\t\t\t" << subtract_args_with_fold_2( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
-				}
-
-				std::cout << r2::linefeed;
-
-				{
-					std::cout << "\t\t" << "Test : ( ( ( ( ( ( ( ( ( 100 - 1 ) - 2 ) - 3 ) - 4 ) - 5 ) - 6 ) - 7 ) - 8 ) - 9 );" << r2::linefeed;
-
-					std::cout << "\t\t\t" << ( ( ( ( ( ( ( ( ( 100 - 1 ) - 2 ) - 3 ) - 4 ) - 5 ) - 6 ) - 7 ) - 8 ) - 9 ) << r2::linefeed;
+					std::cout << r2::tab3 << ( 100 - ( 1 - ( 2 - ( 3 - ( 4 - ( 5 - ( 6 - ( 7 - ( 8 - 9 ) ) ) ) ) ) ) ) ) << r2::linefeed;
 				}
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ int subtract_args_with_fold_3( I integer, IntegerList... integer_list )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( integer_list - ... - integer );" << r2::linefeed;
+				std::cout << r2::tab << "+ int subtract_args_with_fold_2(IntegerList... integer_list )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( ... - integer_list );" << r2::linefeed;
 				std::cout << r2::linefeed;
 
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_3( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
+				{
+					std::cout << r2::tab2 << "Call : subtract_args_with_fold_2( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
 
-				std::cout << "\t\t\t" << subtract_args_with_fold_4( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
+					std::cout << r2::tab3 << subtract_args_with_fold_2( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
+				}
+
+				std::cout << r2::linefeed;
+
+				{
+					std::cout << r2::tab2 << "Test : ( ( ( ( ( ( ( ( ( 100 - 1 ) - 2 ) - 3 ) - 4 ) - 5 ) - 6 ) - 7 ) - 8 ) - 9 );" << r2::linefeed;
+
+					std::cout << r2::tab3 << ( ( ( ( ( ( ( ( ( 100 - 1 ) - 2 ) - 3 ) - 4 ) - 5 ) - 6 ) - 7 ) - 8 ) - 9 ) << r2::linefeed;
+				}
 			}
 
 			std::cout << r2::split;
 
 			{
-				std::cout << "\t" << "+ int subtract_args_with_fold_4( I integer, IntegerList... integer_list )" << r2::linefeed;
-				std::cout << "\t\t" << "return ( integer - ... - integer_list );" << r2::linefeed;
+				std::cout << r2::tab << "+ int subtract_args_with_fold_3( I integer, IntegerList... integer_list )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( integer_list - ... - integer );" << r2::linefeed;
 				std::cout << r2::linefeed;
 
-				std::cout << "\t\t" << "Call : subtract_args_with_fold_4( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
+				std::cout << r2::tab2 << "Call : subtract_args_with_fold_3( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
 
-				std::cout << "\t\t\t" << subtract_args_with_fold_3( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
+				std::cout << r2::tab3 << subtract_args_with_fold_4( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
+			}
+
+			std::cout << r2::split;
+
+			{
+				std::cout << r2::tab << "+ int subtract_args_with_fold_4( I integer, IntegerList... integer_list )" << r2::linefeed;
+				std::cout << r2::tab2 << "return ( integer - ... - integer_list );" << r2::linefeed;
+				std::cout << r2::linefeed;
+
+				std::cout << r2::tab2 << "Call : subtract_args_with_fold_4( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 )" << r2::linefeed;
+
+				std::cout << r2::tab3 << subtract_args_with_fold_3( 100, 1, 2, 3, 4, 5, 6, 7, 8, 9 ) << r2::linefeed;
 			}
 
 			std::cout << r2::split;
