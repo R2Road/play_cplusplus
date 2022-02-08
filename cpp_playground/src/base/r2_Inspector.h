1ad5f7a455ea0cc80e8a952c2e4be02889f7c1fb
r2road <studior2road@gmail.com>
2022-02-08 09:24:31 +0900

    for ez

--- cpp_playground/src/base/r2_Inspector.h
+++ cpp_playground/src/base/r2_Inspector.h
@@ -2,52 +2,52 @@
 
 #define	EXPECT_TRUE( condition )									\
 do {																\
-	if( !( condition ) )											\
+	if( ( condition ) )												\
 	{																\
-		__debugbreak();												\
-		printf( "FAILED : EXPECT_TRUE : %s\n", , #condition );		\
+		printf( "PASS : EXPECT_TRUE : %s\n", #condition );			\
 	}																\
 	else															\
 	{																\
-		printf( "PASS : EXPECT_TRUE : %s\n", #condition );			\
+		__debugbreak();												\
+		printf( "FAILED : EXPECT_TRUE : %s\n", , #condition );		\
 	}																\
 } while( false )
 
 #define	EXPECT_FALSE( condition )									\
 do {																\
-	if( ( condition ) )												\
+	if( !( condition ) )												\
 	{																\
-		__debugbreak();												\
-		printf( "FAILED : EXPECT_FALSE : %s\n", #condition );		\
+		printf( "PASS : EXPECT_FALSE : %s\n", #condition );			\
 	}																\
 	else															\
 	{																\
-		printf( "PASS : EXPECT_FALSE : %s\n", #condition );			\
+		__debugbreak();												\
+		printf( "FAILED : EXPECT_FALSE : %s\n", #condition );		\
 	}																\
 } while( false )
 
 #define	EXPECT_EQ( condition_1, condition_2 )											\
 do {																					\
-	if( ( condition_1 ) != ( condition_2 ) )											\
+	if( ( condition_1 ) == ( condition_2 ) )											\
+	{																					\
+		printf( "PASS : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
+	}																					\
+	else																				\
 	{																					\
 		__debugbreak();																	\
 		printf( "FAILED : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );		\
 	}																					\
-	else																				\
+} while( false )
+
+#define	EXPECT_NQ( condition_1, condition_2 )											\
+do {																					\
+	if( ( condition_1 ) != ( condition_2 ) )											\
 	{																					\
 		printf( "PASS : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
 	}																					\
-} while( false )
-
-#define	EXPECT_NEQ( condition_1, condition_2 )											\
-do {																					\
-	if( ( condition_1 ) == ( condition_2 ) )											\
+	else																				\
 	{																					\
 		__debugbreak();																	\
 		printf( "FAILED : EXPECT_EQ : %s != %s\n", #condition_1, #condition_2 );		\
-	}																					\
-	else																				\
-	{																					\
-		printf( "PASS : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
 	}																					\
 } while( false )
