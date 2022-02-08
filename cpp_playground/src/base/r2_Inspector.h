6c34e6f1150e79fc631f22601aa105ced6dbd49b
r2road <studior2road@gmail.com>
2022-02-08 12:00:24 +0900

    update : r2 - Inspector

--- cpp_playground/src/base/r2_Inspector.h
+++ cpp_playground/src/base/r2_Inspector.h
@@ -4,12 +4,12 @@
 do {																\
 	if( ( condition ) )												\
 	{																\
-		printf( "PASS : EXPECT_TRUE : %s\n", #condition );			\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_TRUE : %s\n", #condition );			\
 	}																\
 	else															\
 	{																\
 		__debugbreak();												\
-		printf( "FAILED : EXPECT_TRUE : %s\n", , #condition );		\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" "  : EXPECT_TRUE : %s\n", , #condition );		\
 	}																\
 } while( false )
 
@@ -17,12 +17,12 @@ do {																\
 do {																\
 	if( !( condition ) )												\
 	{																\
-		printf( "PASS : EXPECT_FALSE : %s\n", #condition );			\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_FALSE : %s\n", #condition );			\
 	}																\
 	else															\
 	{																\
 		__debugbreak();												\
-		printf( "FAILED : EXPECT_FALSE : %s\n", #condition );		\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_FALSE : %s\n", #condition );		\
 	}																\
 } while( false )
 
@@ -30,12 +30,12 @@ do {																\
 do {																					\
 	if( ( condition_1 ) == ( condition_2 ) )											\
 	{																					\
-		printf( "PASS : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
 	}																					\
 	else																				\
 	{																					\
 		__debugbreak();																	\
-		printf( "FAILED : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );		\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );		\
 	}																					\
 } while( false )
 
@@ -43,11 +43,63 @@ do {																					\
 do {																					\
 	if( ( condition_1 ) != ( condition_2 ) )											\
 	{																					\
-		printf( "PASS : EXPECT_EQ : %s == %s\n", #condition_1, #condition_2 );			\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_NQ : %s != %s\n", #condition_1, #condition_2 );			\
 	}																					\
 	else																				\
 	{																					\
 		__debugbreak();																	\
-		printf( "FAILED : EXPECT_EQ : %s != %s\n", #condition_1, #condition_2 );		\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_NQ : %s != %s\n", #condition_1, #condition_2 );		\
+	}																					\
+} while( false )
+
+#define	EXPECT_GT( condition_1, condition_2 )											\
+do {																					\
+	if( ( condition_1 ) > ( condition_2 ) )											\
+	{																					\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_GT : %s > %s\n", #condition_1, #condition_2 );			\
+	}																					\
+	else																				\
+	{																					\
+		__debugbreak();																	\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_GT : %s > %s\n", #condition_1, #condition_2 );		\
+	}																					\
+} while( false )
+
+#define	EXPECT_LT( condition_1, condition_2 )											\
+do {																					\
+	if( ( condition_1 ) < ( condition_2 ) )											\
+	{																					\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_LT : %s < %s\n", #condition_1, #condition_2 );			\
+	}																					\
+	else																				\
+	{																					\
+		__debugbreak();																	\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_LT : %s < %s\n", #condition_1, #condition_2 );		\
+	}																					\
+} while( false )
+
+#define	EXPECT_GE( condition_1, condition_2 )											\
+do {																					\
+	if( ( condition_1 ) >= ( condition_2 ) )											\
+	{																					\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_GE : %s >= %s\n", #condition_1, #condition_2 );			\
+	}																					\
+	else																				\
+	{																					\
+		__debugbreak();																	\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_GE : %s >= %s\n", #condition_1, #condition_2 );		\
+	}																					\
+} while( false )
+
+#define	EXPECT_LE( condition_1, condition_2 )											\
+do {																					\
+	if( ( condition_1 ) <= ( condition_2 ) )											\
+	{																					\
+		printf( "\x1B[92m" "[PASS]" "\033[0m" " : EXPECT_EQ : %s <= %s\n", #condition_1, #condition_2 );			\
+	}																					\
+	else																				\
+	{																					\
+		__debugbreak();																	\
+		printf( "\x1B[91m" "[FAILED]" "\033[0m" " : EXPECT_EQ : %s <= %s\n", #condition_1, #condition_2 );		\
 	}																					\
 } while( false )
