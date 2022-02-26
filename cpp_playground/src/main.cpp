14895068697fd39afbd4654bed80c4d9448e7332
r2road <studior2road@gmail.com>
2022-02-27 03:22:38 +0900

    move console window : 0, 0

--- cpp_playground/src/main.cpp
+++ cpp_playground/src/main.cpp
@@ -24,10 +24,25 @@ int main()
 	// Environment : Position
 	//
 	{
+		//
+		// # MoveWindow( hWnd, 0, 0... ); 만으로는 Window 가 실질적인 0, 0 좌표에 위치하지 않는다.
+		// # 아래의 코드를 이용해서 타이틀 바와 얇은 프레임을 제거 해야만 위 코드로 0, 0 위치에 보여진다.
+		//		LONG lStyle = GetWindowLong( GetConsoleWindow(), GWL_STYLE );
+		//		lStyle &= ~( WS_CAPTION | WS_THICKFRAME );
+		//		SetWindowLong( GetConsoleWindow(), GWL_STYLE, lStyle );
+		//
+		// # 내 목표는 타이틀 바는 남아 있지만 Window 가 0, 0 에 자리하는 것이다.
+		// # 따라서 Frame 크기를 계산해서 강제로 0, 0 위치에 보이게 하고자 한다.
+		//
+		int offset_x = 0;
+		offset_x -= GetSystemMetrics( SM_CXBORDER );
+		offset_x -= GetSystemMetrics( SM_CXFRAME );
+		offset_x -= GetSystemMetrics( SM_CXFIXEDFRAME );
+
 		HWND hWnd = GetConsoleWindow();
 		RECT rectClient;
 		GetClientRect( hWnd, &rectClient );
-		MoveWindow( hWnd, 0, 0, rectClient.right - rectClient.left, rectClient.bottom - rectClient.top, TRUE );
+		MoveWindow( hWnd, offset_x, 0, rectClient.right - rectClient.left, rectClient.bottom - rectClient.top, TRUE );
 	}
 
 	//
