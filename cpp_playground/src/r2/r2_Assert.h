7c99715a2e3971b6b63baf7d8fb90c65ce57a03d
r2road <studior2road@gmail.com>
2022-04-22 07:50:37 +0900

    adjust encoding

--- cpp_playground/src/r2/r2_Assert.h
+++ cpp_playground/src/r2/r2_Assert.h
@@ -1,16 +1,16 @@
-﻿#ifndef R2ASSERT_H
+#ifndef R2ASSERT_H
 #define R2ASSERT_H
 
 #include <stdio.h>
 
-#define	R2ASSERT( condition, message )																										\
-do {																																		\
-	if( !( condition ) )																													\
-	{																																		\
-		printf( "+ R2ASSERT" "\nMessage : %s" "\nFile : %s" "\nLine : %d" "\nFunction : %s", message, __FILE__, __LINE__, __FUNCTION__ );	\
-		__debugbreak();																														\
-		throw std::runtime_error( message );																								\
-	}																																		\
+#define	R2ASSERT( condition, message )		\
+do {										\
+	if( !( condition ) )					\
+	{										\
+		printf( "+ R2ASSERT" "\nCondition : %s" "\nMessage : %s", #condition, message );	\
+		__debugbreak();\
+		throw std::runtime_error( message );\
+	}										\
 } while( false )
 
 
