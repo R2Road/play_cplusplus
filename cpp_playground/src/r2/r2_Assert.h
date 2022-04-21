e587a879f3ebf33be9d7e140d5b1cf304d1647e3
r2road <studior2road@gmail.com>
2022-04-22 07:53:25 +0900

    ...

--- cpp_playground/src/r2/r2_Assert.h
+++ cpp_playground/src/r2/r2_Assert.h
@@ -7,7 +7,7 @@
 do {										\
 	if( !( condition ) )					\
 	{										\
-		printf( "+ R2ASSERT" "\nCondition : %s" "\nMessage : %s", #condition, message );	\
+		printf( "[R2ASSERT]" "\nCondition : %s" "\nMessage : %s", #condition, message );	\
 		__debugbreak();\
 		throw std::runtime_error( message );\
 	}										\
