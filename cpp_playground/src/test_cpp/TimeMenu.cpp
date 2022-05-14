ae20a218cabca05990ab82f03b9fad7afac8a1e9
r2road <studior2road@gmail.com>
2022-05-14 10:00:29 +0900

    rename x 2

--- cpp_playground/src/test_cpp/TimeMenu.cpp
+++ cpp_playground/src/test_cpp/TimeMenu.cpp
@@ -1,52 +1,50 @@
 #include "TimeMenu.h"
 
-#include "r2cm/r2cm_Director.h"
 #include "r2cm/r2cm_constant.h"
-#include "RootMenu.h"
+#include "r2cm/r2cm_Director.h"
 
 #include "test_cpp/item/c_time_test.h"
 #include "test_cpp/item/std_chrono_test.h"
 #include "test_windows/item/windows_time_test.h"
 
-namespace r2
+#include "RootMenu.h"
+
+r2cm::MenuUp TimeMenu::Create( r2cm::Director& director )
 {
-	r2cm::MenuUp TimeMenu::Create( r2cm::Director& director )
+	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu( director, GetTitle() ) );
+
 	{
-		r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu( director, GetTitle() ) );
-
-		{
-			ret->AddItem( '1', c_time_test::Basic::GetInstance() );
-			ret->AddItem( '2', c_time_test::Y_M_D_H_S::GetInstance() );
-			ret->AddItem( '3', c_time_test::Clock::GetInstance() );
+		ret->AddItem( '1', c_time_test::Basic::GetInstance() );
+		ret->AddItem( '2', c_time_test::Y_M_D_H_S::GetInstance() );
+		ret->AddItem( '3', c_time_test::Clock::GetInstance() );
 
 
-			ret->AddLineFeed();
+		ret->AddLineFeed();
 
 
-			ret->AddItem( 'q', window_time_test::Basic::GetInstance() );
+		ret->AddItem( 'q', window_time_test::Basic::GetInstance() );
 
 
-			ret->AddLineFeed();
+		ret->AddLineFeed();
 
 
-			ret->AddItem( 'w', std_chrono_test::Epoch::GetInstance() );
-			ret->AddItem( 'e', std_chrono_test::Chrono::GetInstance() );
+		ret->AddItem( 'w', std_chrono_test::Epoch::GetInstance() );
+		ret->AddItem( 'e', std_chrono_test::Chrono::GetInstance() );
 
 
-			ret->AddSplit();
+		ret->AddSplit();
 
 
-			ret->AddItem(
-				27
-				, []()->const char* { return "Return To Root"; }
-				, [&director]()->r2cm::eItemLeaveAction
-				{
-					director.Setup( RootMenu::Create( director ) );
-					return r2cm::eItemLeaveAction::None;
-				}
-			);
-		}
-
-		return ret;
+		ret->AddItem(
+			27
+			, []()->const char* { return "Return To Root"; }
+			, [&director]()->r2cm::eItemLeaveAction
+			{
+				director.Setup( RootMenu::Create( director ) );
+				return r2cm::eItemLeaveAction::None;
+			}
+		);
 	}
+
+	return ret;
 }
--- cpp_playground/src/test_cpp/TimeMenu.h
+++ cpp_playground/src/test_cpp/TimeMenu.h
@@ -5,19 +5,15 @@
 namespace r2cm
 {
 	using MenuUp = std::unique_ptr<class Menu>;
-
 	class Director;
 }
 
-namespace r2
+class TimeMenu
 {
-	class TimeMenu
-	{
-	private:
-		TimeMenu() = delete;
+private:
+	TimeMenu() = delete;
 
-	public:
-		static const char* GetTitle() { return "Time"; }
-		static r2cm::MenuUp Create( r2cm::Director& director );
-	};
-}
+public:
+	static const char* GetTitle() { return "Time"; }
+	static r2cm::MenuUp Create( r2cm::Director& director );
+};
