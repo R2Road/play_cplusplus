12e25b1c6547ad6559c39f04267a8f750466c8f9
r2road <studior2road@gmail.com>
2022-05-23 00:25:00 +0900

    apply : r2cm - Inspector

--- cpp_playground/src/test_algorithm/item/algorithm_maze_test.cpp
+++ cpp_playground/src/test_algorithm/item/algorithm_maze_test.cpp
@@ -3,6 +3,7 @@
 #include <conio.h> // _kbhit(), _getch()
 
 #include "r2cm/r2cm_constant.h"
+#include "r2cm/r2cm_Inspector.h"
 
 #include "r2/r2_Direction4.h"
 #include "r2/r2_Grid.h"
@@ -49,23 +50,26 @@ namespace algorithm_maze_test
 
 			std::cout << r2cm::split;
 
-			r2::Direction4 current_direction;
-			r2::PointInt current_point{ 0, 0 };
-			r2::PointInt temp_point{ 0, 0 };
-			r2::Grid<int> grid;
-			grid.Reset( 19, 19 );
+			DECLARATION_MAIN( r2::Direction4 current_direction );
+			DECLARATION_MAIN( r2::PointInt current_point( 0, 0 ) );
+			DECLARATION_MAIN( r2::PointInt next_point( 0, 0 ) );
+			DECLARATION_MAIN( r2::Grid<int> grid );
+
+			std::cout << r2cm::split;
 
 			{
-				grid.Set( current_point.GetX(), current_point.GetY(), true );
+				PROCESS_MAIN( grid.Reset( 19, 19 ) );
+				PROCESS_MAIN( grid.Set( current_point.GetX(), current_point.GetY(), true ) );
+
+				std::cout << r2cm::linefeed;
 
 				const auto pivot_point = r2cm::WindowUtility::GetCursorPoint();
 				int stay_count = 0;
 				while( 4 >= stay_count )
 				{
-					temp_point.SetX( current_point.GetX() + current_direction.GetPoint().GetX() );
-					temp_point.SetY( current_point.GetY() + current_direction.GetPoint().GetY() );
+					next_point = current_point + current_direction.GetPoint();
 
-					if( !grid.IsIn( temp_point.GetX(), temp_point.GetY() ) ||  0 < grid.Get( temp_point.GetX(), temp_point.GetY() ) )
+					if( !grid.IsIn( next_point.GetX(), next_point.GetY() ) ||  0 < grid.Get( next_point.GetX(), next_point.GetY() ) )
 					{
 						current_direction.Rotate( true );
 						++stay_count;
@@ -73,11 +77,14 @@ namespace algorithm_maze_test
 						continue;
 					}
 
-					grid.Set( temp_point.GetX(), temp_point.GetY(), 1 );
+					grid.Set( next_point.GetX(), next_point.GetY(), 1 );
 
-					current_point = temp_point;
+					current_point = next_point;
 					stay_count = 0;
 
+					//
+					// Output
+					//
 					r2cm::WindowUtility::MoveCursorPoint( pivot_point );
 					ShowGrid( grid );
 					std::cout << r2cm::linefeed;
