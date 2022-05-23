c1119711abf3754390b9e992f7c389bf1be97652
r2road <studior2road@gmail.com>
2022-05-24 03:10:20 +0900

    adjust key map

--- cpp_playground/src/test_windows/ConsoleTextColorMenu.cpp
+++ cpp_playground/src/test_windows/ConsoleTextColorMenu.cpp
@@ -16,15 +16,15 @@ r2cm::MenuUp ConsoleTextColorMenu::Create( r2cm::Director& director )
 	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu( director, GetTitle() ) );
 
 	{
-		ret->AddItem( 'q', console_text_color_test::ColorTable::GetInstance() );
-		ret->AddItem( 'w', console_text_color_test::TextColor1::GetInstance() );
-		ret->AddItem( 'e', console_text_color_test::TextColor2::GetInstance() );
-		ret->AddItem( 'r', console_text_color_test::AdjustColorTable::GetInstance() );
+		ret->AddItem( '1', console_text_color_test::ColorTable::GetInstance() );
+		ret->AddItem( '2', console_text_color_test::TextColor1::GetInstance() );
+		ret->AddItem( '3', console_text_color_test::TextColor2::GetInstance() );
+		ret->AddItem( '4', console_text_color_test::AdjustColorTable::GetInstance() );
 
 		ret->AddLineFeed();
 
-		ret->AddItem( 't', console_text_color_test::ColorTable2::GetInstance() );
-		ret->AddItem( 'y', console_text_color_test::TextColor3::GetInstance() );
+		ret->AddItem( 'q', console_text_color_test::ColorTable2::GetInstance() );
+		ret->AddItem( 'w', console_text_color_test::TextColor3::GetInstance() );
 
 
 		ret->AddSplit();
