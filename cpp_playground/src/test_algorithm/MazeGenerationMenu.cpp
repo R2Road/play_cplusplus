fc97187f057dcca31075e3159ad73b1aa3b62b2a
r2road <studior2road@gmail.com>
2022-06-02 00:12:46 +0900

    add test : test_algorithm - maze_generation_kruskals_test

--- cpp_playground/src/test_algorithm/MazeGenerationMenu.cpp
+++ cpp_playground/src/test_algorithm/MazeGenerationMenu.cpp
@@ -4,6 +4,7 @@
 #include "r2cm/r2cm_Director.h"
 
 #include "test_algorithm/item/algorithm_maze_test.h"
+#include "test_algorithm/item/maze_generation_kruskals_test.h"
 
 #include "AlgorithmMenu.h"
 
@@ -13,6 +14,12 @@ r2cm::MenuUp MazeGenerationMenu::Create( r2cm::Director& director )
 
 	{
 		ret->AddItem( '1', algorithm_maze_test::Basic::GetInstance() );
+
+
+		ret->AddLineFeed();
+
+
+		ret->AddItem( 'q', maze_generation_kruskals_test::Basic::GetInstance() );
 
 
 		ret->AddSplit();
---
+++ cpp_playground/src/test_algorithm/item/maze_generation_kruskals_test.cpp
@@ -0,0 +1,31 @@
+#include "maze_generation_kruskals_test.h"
+
+#include "r2cm/r2cm_constant.h"
+#include "r2cm/r2cm_Inspector.h"
+
+namespace maze_generation_kruskals_test
+{
+	r2cm::iItem::TitleFuncT Basic::GetTitleFunction() const
+	{
+		return []()->const char*
+		{
+			return "Maze : Basic";
+		};
+	}
+	r2cm::iItem::DoFuncT Basic::GetDoFunction()
+	{
+		return []()->r2cm::eItemLeaveAction
+		{
+			std::cout << "# " << GetInstance().GetTitleFunction()( ) << " #" << r2cm::linefeed2;
+
+			std::cout << r2cm::split;
+
+			{
+			}
+
+			std::cout << r2cm::split;
+
+			return r2cm::eItemLeaveAction::Pause;
+		};
+	}
+}
