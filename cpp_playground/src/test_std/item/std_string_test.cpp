689fc817488aadeb45cb626e9623ab8810d67f4c
r2road <studior2road@gmail.com>
2022-07-10 00:22:56 +0900

    update test : test_std - std_string_test - Find_And_Split

--- cpp_playground/src/test_std/item/std_string_test.cpp
+++ cpp_playground/src/test_std/item/std_string_test.cpp
@@ -67,6 +67,40 @@ namespace std_string_test
 
 
 
+	void StringSplit( const std::string& str )
+	{
+		std::cout << r2cm::linefeed;
+
+		DECLARATION_MAIN( const std::string key_string = "\n" );
+		OUTPUT_VALUE( key_string.size() );
+
+		std::cout << r2cm::linefeed;
+
+		OUTPUT_VALUE( str.size() );
+
+		std::cout << r2cm::linefeed;
+
+		DECLARATION_MAIN( std::size_t pivot_pos = 0u );
+		DECLARATION_MAIN( std::size_t find_pos = 0u; );
+
+		int i = 0;
+		while( std::string::npos != find_pos )
+		{
+			find_pos = str.find( key_string, pivot_pos );
+			if( std::string::npos == find_pos )
+			{
+				break;
+			}
+
+			std::cout << r2cm::tab << i << "[" << pivot_pos << " ~ " << find_pos << "]" << " > ";
+			std::cout.write( &str[pivot_pos], find_pos - pivot_pos );
+			std::cout << r2cm::linefeed;
+
+			pivot_pos = find_pos + key_string.size();
+
+			++i;
+		}
+	}
 	r2cm::iItem::TitleFunctionT Find_And_Split::GetTitleFunction() const
 	{
 		return []()->const char*
@@ -82,38 +116,11 @@ namespace std_string_test
 
 			std::cout << r2cm::split;
 
-			DECLARATION_MAIN( const std::string key_string = "\n" );
-			OUTPUT_VALUE( key_string.size() );
-
-			std::cout << r2cm::linefeed;
-
-			DECLARATION_MAIN( const std::string orig_string = "\n" "\n" "012345" "\n" "6789abcd" "\n" "\n" "efghij" "\n" "klmnopqrstu" "\n" "\n" "\n" "vwxyz" "\n" );
-			OUTPUT_VALUE( orig_string.size() );
+			PROCESS_MAIN( StringSplit( "\n" "\n" "012345" "\n" "6789abcd" "\n" "\n" "efghij" "\n" "klmnopqrstu" "\n" "\n" "\n" "vwxyz" "\n" ) );
 
 			std::cout << r2cm::split;
 
-			{
-				DECLARATION_MAIN( std::size_t pivot_pos = 0u );
-				DECLARATION_MAIN( std::size_t find_pos = 0u; );
-
-				int i = 0;
-				while( std::string::npos != find_pos )
-				{
-					find_pos = orig_string.find( key_string, pivot_pos );
-					if( std::string::npos == find_pos )
-					{
-						break;
-					}
-
-					std::cout << r2cm::tab << i << "[" << pivot_pos << " ~ " << find_pos << "]" << " > ";
-					std::cout.write( &orig_string[pivot_pos], find_pos - pivot_pos );
-					std::cout << r2cm::linefeed;
-
-					pivot_pos = find_pos + key_string.size();
-
-					++i;
-				}
-			}
+			PROCESS_MAIN( StringSplit( "\n" "\n" "123" "\n" "456" "\n" "\n" "789" "\n" "abc" "\n" "\n" "\n" "defg" ) );
 
 			std::cout << r2cm::split;
 
