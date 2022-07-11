58361051450739369ed4e4b3f3770670672aea08
r2road <studior2road@gmail.com>
2022-07-11 17:15:59 +0900

    ...

--- cpp_playground/src/RootMenu.cpp
+++ cpp_playground/src/RootMenu.cpp
@@ -38,132 +38,44 @@ r2cm::MenuUp RootMenu::Create( r2cm::Director& director )
 
 	{
 		ret->AddItem( '1', console_input_test::Basic::GetInstance() );
-		ret->AddItem(
-			'2'
-			, []()->const char* { return WindowsMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( WindowsMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);		
+		ret->AddMenu<WindowsMenu>( '2' );		
+
 
 
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'q'
-			, []()->const char* { return C_CPP_Menu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( C_CPP_Menu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'w'
-			, []()->const char* { return STDMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( STDMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'e'
-			, []()->const char* { return FileMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( FileMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'r'
-			, []()->const char* { return TimeMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( TimeMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			't'
-			, []()->const char* { return RandomMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( RandomMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'y'
-			, []()->const char* { return DebugMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( DebugMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+
+		ret->AddMenu<C_CPP_Menu>( 'q' );
+		ret->AddMenu<STDMenu>( 'w' );
+		ret->AddMenu<FileMenu>( 'e' );
+		ret->AddMenu<TimeMenu>( 'r' );
+		ret->AddMenu<RandomMenu>( 't' );
+		ret->AddMenu<DebugMenu>( 'y' );
+
 
 
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'a'
-			, []()->const char* { return ETCMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( ETCMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			's'
-			, []()->const char* { return MathMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( MathMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+
+		ret->AddMenu<ETCMenu>( 'a' );
+		ret->AddMenu<MathMenu>( 's' );
+
 
 
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'z'
-			, []()->const char* { return AlgorithmMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( AlgorithmMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'x'
-			, []()->const char* { return HobbyMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( HobbyMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'c'
-			, []()->const char* { return PerformanceMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-				{
-					director.Setup( PerformanceMenu::Create( director ) );
-					return r2cm::eItemLeaveAction::None;
-				}
-		);
+
+		ret->AddMenu<AlgorithmMenu>( 'z' );
+		ret->AddMenu<HobbyMenu>( 'x' );
+		ret->AddMenu<PerformanceMenu>( 'c' );
+
 
 
 		ret->AddSplit();
+
 
 
 		ret->AddItem(
