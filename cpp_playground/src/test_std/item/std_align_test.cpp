da07cb83a6f2cab6527ebf6cdf77d097846c8b3a
r2road <studior2road@gmail.com>
2022-07-16 22:20:52 +0900

    ...

--- cpp_playground/src/test_std/item/std_align_test.cpp
+++ cpp_playground/src/test_std/item/std_align_test.cpp
@@ -23,12 +23,12 @@ namespace align_test
 			std::cout << r2cm::split;
 
 			{
-				std::cout << r2cm::tab << "+ Summury : std::align의 처리 순서" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "> 1. 지정한 bound 에 맞게 메모리 정렬이 가능한지 검토한다." << r2cm::linefeed;
+				std::cout << r2cm::tab << "+ Summury : std::align의 처리 순서" << r2cm::linefeed2;
+				std::cout << r2cm::tab << "> 1. 지정한 bound 에 맞게 메모리 정렬이 가능한지 검토한다." << r2cm::linefeed;
 				std::cout << r2cm::tab2 << "char가 3개 사용된 메모리 위치 에서 int 값을 요청하면 남은 공간에서 메모리 정렬이 가능한가" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "> 2. 메모리 정렬 완료후 남은 메모리 공간이 원하는 T 의 크기를 만족하는지 확인" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "> 3. 이동" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "> 4. 정렬이 필요 없고 남은 공간도 충분하다면 변하는 것이 없다." << r2cm::linefeed;
+				std::cout << r2cm::tab << "> 2. 메모리 정렬 완료후 남은 메모리 공간이 원하는 T 의 크기를 만족하는지 확인" << r2cm::linefeed;
+				std::cout << r2cm::tab << "> 3. 이동" << r2cm::linefeed;
+				std::cout << r2cm::tab << "> 4. 정렬이 필요 없고 남은 공간도 충분하다면 변하는 것이 없다." << r2cm::linefeed;
 			}
 
 			std::cout << r2cm::split;
