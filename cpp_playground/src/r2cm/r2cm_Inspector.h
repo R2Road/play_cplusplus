03daf8d4e3b5e98764541fdaaf33dc5fe2860491
r2road <studior2road@gmail.com>
2022-07-20 00:10:43 +0900

    update r2cm

--- cpp_playground/src/r2cm/r2cm_Inspector.h
+++ cpp_playground/src/r2cm/r2cm_Inspector.h
@@ -3,6 +3,15 @@
 #include <iostream>
 #include <stdint.h>
 
+#define R2_ENABLE_DEBUG_BREAK 1
+
+#if defined( R2_ENABLE_DEBUG_BREAK ) && R2_ENABLE_DEBUG_BREAK == 1
+	#define R2_DEBUG_BREAK ( __debugbreak() )
+#else
+	#define R2_DEBUG_BREAK
+#endif // R2_ENABLE_DEBUG_BREAK
+
+
 #define	EXPECT_TRUE( condition )																\
 do {																							\
 	if( ( condition ) )																			\
@@ -11,7 +20,7 @@ do {																							\
 	}																							\
 	else																						\
 	{																							\
-		__debugbreak();																			\
+		R2_DEBUG_BREAK;																			\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_TRUE( %s )\n", #condition );			\
 	}																							\
 } while( false )
@@ -24,7 +33,7 @@ do {																							\
 	}																							\
 	else																						\
 	{																							\
-		__debugbreak();																			\
+		R2_DEBUG_BREAK;																			\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_FALSE( %s )\n", #condition );			\
 	}																							\
 } while( false )
@@ -37,7 +46,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_EQ( %s == %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -50,7 +59,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_NE( %s != %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -63,7 +72,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_GT( %s > %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -76,7 +85,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_LT( %s < %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -89,7 +98,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_GE( %s >= %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -102,7 +111,7 @@ do {																											\
 	}																											\
 	else																										\
 	{																											\
-		__debugbreak();																							\
+		R2_DEBUG_BREAK;																							\
 		printf( "\x1B[91m" "[FAILED]" "\033[0m" " EXPECT_LE( %s <= %s )\n", #condition_1, #condition_2 );		\
 	}																											\
 } while( false )
@@ -183,6 +192,46 @@ void SHOW_BINARY( const T value )
 			const int32_t temp_2 = temp_1 & 1;
 
 			std::cout << temp_2;
+		}
+	}
+}
+
+//
+// + exalple : int a[4];
+// pointer : int*
+// size : 4
+//
+#define	OUTPUT_BINARIES( pointer, size )													\
+do {																						\
+	printf( "[BINARIES]" " %s" ", %s" "\n", #pointer, #size );								\
+	SHOW_BINARY( ( pointer ), ( size ) );													\
+	std::cout << "\n";																		\
+} while( false )
+template<typename T>
+void SHOW_BINARY( const T* p, const uint64_t size )
+{
+	const uint64_t fixed_limit = sizeof( T ) * size;
+	const uint8_t* fixed_p = reinterpret_cast<const uint8_t*>( p );
+
+	uint64_t lf_cnt = 0;
+	for( uint64_t i = 0; fixed_limit > i; ++i )
+	{
+		if( 0 == lf_cnt )
+		{
+			printf( "\t> " );
+		}
+
+		SHOW_BINARY( fixed_p[i] );
+
+		++lf_cnt;
+		if( 8 == lf_cnt && fixed_limit > ( i + 1 ) )
+		{
+			lf_cnt = 0;
+			printf( "\n" );
+		}
+		else
+		{
+			printf( " " );
 		}
 	}
 }
