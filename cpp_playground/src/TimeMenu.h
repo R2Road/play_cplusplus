50bbe3a62ea884052f9ed6d5ce020ca446fd92da
r2road <studior2road@gmail.com>
2022-07-24 00:34:17 +0900

    ...

--- cpp_playground/src/TimeMenu.h
+++ cpp_playground/src/TimeMenu.h
@@ -14,6 +14,6 @@ private:
 	TimeMenu() = delete;
 
 public:
-	static const char* GetTitle() { return "Time"; }
+	static const char* GetTitle() { return "Time Menu"; }
 	static r2cm::MenuUp Create( r2cm::Director& director );
 };
