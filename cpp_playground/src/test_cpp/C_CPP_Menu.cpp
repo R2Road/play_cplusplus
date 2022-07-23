8f868c51fe601c135692ee5000479d2b99470713
r2road <studior2road@gmail.com>
2022-07-23 12:28:35 +0900

    ...

--- cpp_playground/src/test_cpp/C_CPP_Menu.cpp
+++ cpp_playground/src/test_cpp/C_CPP_Menu.cpp
@@ -27,99 +27,27 @@ r2cm::MenuUp C_CPP_Menu::Create( r2cm::Director& director )
 	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu( director, C_CPP_Menu::GetTitle() ) );
 
 	{
-		ret->AddItem(
-			'1'
-			, []()->const char* { return BitOperationMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( BitOperationMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<BitOperationMenu>( '1' );
 		ret->AddItem( '2', array_test::Init::GetInstance() );
-		ret->AddItem(
-			'3'
-			, []()->const char* { return ClassMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( ClassMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<ClassMenu>( '3' );
 		ret->AddItem( '4', c_sizeof_test::Basic::GetInstance() );
 
 
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'5'
-			, []()->const char* { return PointerMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( PointerMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'6'
-			, []()->const char* { return CharMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( CharMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'7'
-			, []()->const char* { return TemplateMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( TemplateMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'8'
-			, []()->const char* { return AlignMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( AlignMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'9'
-			, []()->const char* { return EnumMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( EnumMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<PointerMenu>( '5' );
+		ret->AddMenu<CharMenu>( '6' );
+		ret->AddMenu<TemplateMenu>( '7' );
+		ret->AddMenu<AlignMenu>( '8' );
+		ret->AddMenu<EnumMenu>( '9' );
 
 
 		ret->AddLineFeed();
 
 
-		ret->AddItem(
-			'q'
-			, []()->const char* { return NewMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( NewMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
-		ret->AddItem(
-			'w'
-			, []()->const char* { return LambdaMenu::GetTitle(); }
-			, [&director]()->r2cm::eItemLeaveAction
-			{
-				director.Setup( LambdaMenu::Create( director ) );
-				return r2cm::eItemLeaveAction::None;
-			}
-		);
+		ret->AddMenu<NewMenu>( 'q' );
+		ret->AddMenu<LambdaMenu>( 'w' );
 		ret->AddItem( 'e', structured_binding_test::Basic::GetInstance() );
 		ret->AddItem( 'r', print_test::Basic::GetInstance() );
 		ret->AddItem( 't', print_test::ShowNumbers::GetInstance() );
