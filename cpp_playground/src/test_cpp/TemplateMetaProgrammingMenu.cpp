e8da7b742d9b40783a5ee76bc779ebabeb9d7ea9
r2road <studior2road@gmail.com>
2022-07-26 22:18:47 +0900

    ...

--- cpp_playground/src/test_cpp/TemplateMetaProgrammingMenu.cpp
+++ cpp_playground/src/test_cpp/TemplateMetaProgrammingMenu.cpp
@@ -1,7 +1,7 @@
 #include "TemplateMetaProgrammingMenu.h"
 
-#include "r2cm/r2cm_ostream.h"
 #include "r2cm/r2cm_Director.h"
+#include "r2cm/r2cm_ostream.h"
 
 #include "test_cpp/item/template_meta_programming_01_test.h"
 #include "test_cpp/item/template_meta_programming_02_test.h"
