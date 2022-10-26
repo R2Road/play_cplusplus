e9a24fcded6f09d83d469e66cff99165aefd7a61
R2Road <studior2road@gmail.com>
2022-10-27 04:31:07 +0900

    show binary

--- cpp_playground/src/test_hobby/item/serialize_test.cpp
+++ cpp_playground/src/test_hobby/item/serialize_test.cpp
@@ -18,13 +18,22 @@ namespace serialize_test
 		{
 			std::cout << r2cm::split;
 
-			DECLARATION_MAIN( struct { int64_t a; int64_t b; } dummy );
+			DECLARATION_MAIN( struct { int64_t a = 13; int64_t b = -7; } dummy );
 
 			std::cout << r2cm::split;
 
 			{
 				DECLARATION_MAIN( const auto size = sizeof( dummy ) );
 				OUTPUT_VALUE( size );
+
+				std::cout << r2cm::linefeed;
+
+				DECLARATION_MAIN( uint8_t* up = reinterpret_cast<uint8_t*>( &dummy ) );
+				for( int i = 0; size > i; ++i )
+				{
+					OUTPUT_BINARY( *up );
+					++up;
+				}
 			}
 
 			std::cout << r2cm::split;
