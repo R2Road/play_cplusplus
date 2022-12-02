43b83dee9257c3c4c3976a78da5bbb2b42e9c564
R2Road <studior2road@gmail.com>
2022-12-03 03:09:32 +0900

    update test : test_std - std_algorithm_test - Shuffle

--- cpp_playground/src/test_std/item/std_algorithm_test.cpp
+++ cpp_playground/src/test_std/item/std_algorithm_test.cpp
@@ -5,15 +5,24 @@
 #include <random>
 #include <vector>
 
+#include "r2cm/r2cm_Inspector.h"
 #include "r2cm/r2cm_ostream.h"
 
 namespace std_algorithm_test
 {
+	std::ostream& operator<<( std::ostream& o, std::vector<int>& container )
+	{
+		for( const auto& i : container )
+		{
+			o << i << " ";
+		}
+		return o;
+	}
 	r2cm::iItem::TitleFunctionT Shuffle::GetTitleFunction() const
 	{
 		return []()->const char*
 		{
-			return "Algorithm : Shuffle";
+			return "<algorithm> : std::shuffle";
 		};
 	}
 	r2cm::iItem::DoFunctionT Shuffle::GetDoFunction()
@@ -22,48 +31,22 @@ namespace std_algorithm_test
 		{
 			std::cout << r2cm::split;
 
-			std::vector<int> container_1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-			{
-				std::cout << r2cm::tab << "+ Declaration" << r2cm::linefeed2;
-				std::cout << r2cm::tab2 << "std::vector<int> container_1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };" << r2cm::linefeed;
-			}
+			DECLARATION_MAIN( std::vector<int> container( { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } ) );
 
 			std::cout << r2cm::split;
 
 			{
-				std::cout << r2cm::tab << "+ View" << r2cm::linefeed2;
-				std::cout << r2cm::tab2;
-				for( const auto i : container_1 )
-				{
-					std::cout << i << "  ";
-				}
+				std::cout << r2cm::tab << "- container : " << container << r2cm::linefeed;
+
 				std::cout << r2cm::linefeed;
-			}
 
-			std::cout << r2cm::split;
+				DECLARATION_MAIN( const auto seed = (unsigned int)std::chrono::system_clock::now().time_since_epoch().count() );
+				DECLARATION_MAIN( std::default_random_engine random_engine( seed ) );
+				PROCESS_MAIN( std::shuffle( container.begin(), container.end(), random_engine ) );
 
-			{
-				const auto seed = static_cast<unsigned int>( std::chrono::system_clock::now().time_since_epoch().count() );
-				std::default_random_engine random_engine( seed );
-				std::shuffle( container_1.begin(), container_1.end(), random_engine );
-
-				std::cout << r2cm::tab << "+ Process" << r2cm::linefeed2;
-				std::cout << r2cm::tab2 << "const auto seed = static_cast<unsigned int>( std::chrono::system_clock::now().time_since_epoch().count() );" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "std::default_random_engine random_engine( seed );" << r2cm::linefeed;
-				std::cout << r2cm::tab2 << "std::shuffle( container_1.begin(), container_1.end(), random_engine );" << r2cm::linefeed;
-			}
-
-			std::cout << r2cm::split;
-
-			{
-				std::cout << r2cm::tab << "+ View" << r2cm::linefeed2;
-				std::cout << r2cm::tab2;
-				for( const auto i : container_1 )
-				{
-					std::cout << i << "  ";
-				}
 				std::cout << r2cm::linefeed;
+
+				std::cout << r2cm::tab << "- container : " << container << r2cm::linefeed;
 			}
 
 			std::cout << r2cm::split;
