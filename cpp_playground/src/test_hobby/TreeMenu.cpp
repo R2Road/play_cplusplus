72e0013ce3146c8500d6255895c7751abbb48776
R2Road <studior2road@gmail.com>
2022-12-19 19:10:01 +0900

    add test : test_hobby - hobby_tree_play

--- cpp_playground/src/test_hobby/TreeMenu.cpp
+++ cpp_playground/src/test_hobby/TreeMenu.cpp
@@ -3,7 +3,7 @@
 #include "r2cm/r2cm_Director.h"
 #include "r2cm/r2cm_ostream.h"
 
-#include "item/memory_pool_test.h"
+#include "item/hobby_tree_play.h"
 
 #include "HobbyMenu.h"
 
@@ -12,6 +12,7 @@ r2cm::MenuUp TreeMenu::Create( r2cm::Director& director )
 	r2cm::MenuUp ret( new ( std::nothrow ) r2cm::Menu( director, GetTitle()	) );
 
 	{
+		ret->AddItem( '1', hobby_tree_play::Basic() );
 
 
 
---
+++ cpp_playground/src/test_hobby/item/hobby_tree_play.cpp
@@ -0,0 +1,37 @@
+#include "hobby_tree_play.h"
+
+#include "r2cm/r2cm_Inspector.h"
+#include "r2cm/r2cm_ostream.h"
+
+namespace hobby_tree_play
+{
+	r2cm::iItem::TitleFunctionT Basic::GetTitleFunction() const
+	{
+		return []()->const char*
+		{
+			return "Tree : Basic";
+		};
+	}
+	r2cm::iItem::DoFunctionT Basic::GetDoFunction() const
+	{
+		return []()->r2cm::eItemLeaveAction
+		{
+			std::cout << r2cm::split;
+
+			OUTPUT_NOTE( "Tree 의 특징" );
+			OUTPUT_COMMENT( "그래프의 일종" );
+			OUTPUT_COMMENT( "순환하지 않는 그래프" );
+			OUTPUT_COMMENT( "정점의 개수가 N 일때 연결선이 N - 1 개" );
+
+			std::cout << r2cm::split;
+
+			{
+				std::cout << "뷰어를 먼저 만들어야겠는데... 뭐지 뭔가... 앞뒤가 잘못된 느낌이야." << r2cm::linefeed;
+			}
+
+			std::cout << r2cm::split;
+
+			return r2cm::eItemLeaveAction::Pause;
+		};
+	}
+}
