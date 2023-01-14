68f5d15300fa322eb53fa7876663b3cac263f8c9
R2Road <studior2road@gmail.com>
2023-01-15 02:23:23 +0900

    update test : c_memset_test - Basic

--- cpp_playground/src/test_c/item/c_memset_test.cpp
+++ cpp_playground/src/test_c/item/c_memset_test.cpp
@@ -20,25 +20,25 @@ namespace c_memset_test
 		{
 			std::cout << r2cm::split;
 
-			DECLARATION_MAIN( char buffer[10] = { 0 } );
-			OUTPUT_BINARIES( buffer, 10 );
+			DECLARATION_MAIN( const uint32_t buffer_size = 6 );
+			DECLARATION_MAIN( char buffer[buffer_size] = { 0 } );
+			OUTPUT_BINARIES( buffer, buffer_size );
 
 			std::cout << r2cm::split;
 
 			{
 				PROCESS_MAIN( memset( buffer, 1, sizeof( buffer ) ) );
-				OUTPUT_BINARIES( buffer, 10 );
+				OUTPUT_BINARIES( buffer, buffer_size );
 			}
 
 			std::cout << r2cm::split;
 
 			{
-				PROCESS_MAIN( memset( buffer, 7, sizeof( buffer ) / 2 ) );
-				OUTPUT_BINARIES( buffer, 10 );
+				PROCESS_MAIN( memset( buffer, -1, sizeof( buffer ) / 2 ) );
+				OUTPUT_BINARIES( buffer, buffer_size );
 			}
 
 			std::cout << r2cm::split;
-
 
 			return r2cm::eItemLeaveAction::Pause;
 		};
