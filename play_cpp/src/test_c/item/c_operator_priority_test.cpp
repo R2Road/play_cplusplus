6efac83371d00908c346aba66ca23408c8e85d01
R2Road <studior2road@gmail.com>
2025-04-18 17:36:07 +0900

    update test : c_operator_priority_test : Basic

--- play_cpp/src/test_c/item/c_operator_priority_test.cpp
+++ play_cpp/src/test_c/item/c_operator_priority_test.cpp
@@ -38,6 +38,18 @@ namespace c_operator_priority_test
 
 			LS();
 
+			{
+				OUTPUT_NOTE( "우선 순위가 같은 연산자는 왼쪽 > 오른쪽" );
+
+				LF();
+
+				OUTPUT_VALUE( ( 2.f / 10.f * 2 ) );
+				EXPECT_EQ( ( 2.f / 10.f * 2 ), ( ( 2.f / 10.f ) * 2 ) );
+				EXPECT_NE( ( 2.f / 10.f * 2 ), ( 2.f / ( 10.f * 2 ) ) );
+			}
+
+			LS();
+
 			return r2tm::eDoLeaveAction::Pause;
 		};
 	}
