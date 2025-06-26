93f55661ea27603a16097f5f2e95ae3524ef87cf
R2Road <studior2road@gmail.com>
2025-06-26 11:46:44 +0900

    add test : play_math_rendering_pipeline - ProjectionMatrix_Perspective_Step3

--- play_cpp/src/play_math/play_math_rendering_pipeline.cpp
+++ play_cpp/src/play_math/play_math_rendering_pipeline.cpp
@@ -288,8 +288,8 @@ namespace play_math_rendering_pipeline
 
 				LF();
 
-				OUTPUT_SUBJECT( "Orthographic : 직교 투영" );
-				OUTPUT_COMMENT( "-1 ~ +1 : 로 표현되는 좌표계로 변환" );
+				OUTPUT_SUBJECT( "orthoRH_NO" );
+				OUTPUT_COMMENT( "Orthographic : 직교 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환" );
 			}
 
 			LS();
@@ -373,9 +373,8 @@ namespace play_math_rendering_pipeline
 
 				LF();
 
-				OUTPUT_SUBJECT( "Orthographic : 직교 투영" );
-				OUTPUT_COMMENT( "-1 ~ +1 : 로 표현되는 좌표계로 변환" );
-				OUTPUT_COMMENT( "Z 범위만 0 ~ 1" );
+				OUTPUT_SUBJECT( "orthoRH_ZO" );
+				OUTPUT_COMMENT( "Orthographic : 직교 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환     |     Z 범위만 0 ~ 1" );
 			}
 
 			LS();
@@ -459,8 +458,8 @@ namespace play_math_rendering_pipeline
 
 				LF();
 
-				OUTPUT_SUBJECT( "Perspective : 원근 투영" );
-				OUTPUT_COMMENT( "-1 ~ +1 : 로 표현되는 좌표계로 변환     |     fov 없는 원근 투영     |     더 낮은 수준에서 원근 투영을 제어 할 때 사용" );
+				OUTPUT_SUBJECT( "frustumRH_NO" );
+				OUTPUT_COMMENT( "Perspective : 원근 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환     |     fov 없는 원근 투영     |     더 낮은 수준에서 원근 투영을 제어 할 때 사용" );
 			}
 
 			LS();
@@ -540,8 +539,8 @@ namespace play_math_rendering_pipeline
 
 				LF();
 
-				OUTPUT_SUBJECT( "Perspective : 원근 투영" );
-				OUTPUT_COMMENT( "-1 ~ +1 : 로 표현되는 좌표계로 변환     |     fov 없는 원근 투영     |     더 낮은 수준에서 원근 투영을 제어 할 때 사용" );
+				OUTPUT_SUBJECT( "frustumRH_ZO" );
+				OUTPUT_COMMENT( "Perspective : 원근 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환     |     fov 없는 원근 투영     |     더 낮은 수준에서 원근 투영을 제어 할 때 사용" );
 				OUTPUT_COMMENT( "Z = 0 ~ 1" );
 			}
 
@@ -707,7 +706,7 @@ namespace play_math_rendering_pipeline
 				LF();
 
 				OUTPUT_SUBJECT( "perspectiveRH_NO" );
-				OUTPUT_COMMENT( "원근 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환     |     fovY : 수직 시야각     |     aspect : 뷰포트의 너비 / 높이" );
+				OUTPUT_COMMENT( "Perspective : 원근 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환     |     fovY : 수직 시야각     |     aspect : 뷰포트의 너비 / 높이" );
 			}
 
 			LS();
@@ -728,6 +727,85 @@ namespace play_math_rendering_pipeline
 					, 0.f                           , 1 / tanHalfFovY  , 0.f                               , 0.f
 					, 0.f                           , 0.f              , -( far + near ) / ( far - near )  , -( 2 * far * near ) / ( far - near )
 					, 0.f                           , 0.f              , -1.f                              , 0.f
+				);
+				OUTPUT_SOURCE_END;
+
+				LF();
+
+				OUTPUT_VALUE( projection_mat4 );
+
+				LF();
+
+				OUTPUT_SUBJECT( "행렬과 벡터를 곱하고 나온 결과 벡터의 각 성분을 결과 벡터의 w 값으로 나누어야 원근 조정이 완료된다." );
+				OUTPUT_SUBJECT( "이 과정은 보통 파이프라인 에서 알아서 처리 하기 때문에 유저에겐 보이지 않는다고 한다." );
+
+
+				SS();
+
+
+				{
+					DECLARATION_MAIN( auto v = projection_mat4 * Vec4( 0, 0, far, 1 ) );
+					OUTPUT_VALUE( v );
+					PROCESS_MAIN( v /= v.w );
+					OUTPUT_VALUE( v );
+					EXPECT_EP_EQ( 0.f, v.x );
+					EXPECT_EP_EQ( 0.f, v.y );
+					EXPECT_EP_NE( 1.f, v.z );
+
+					LF();
+
+					OUTPUT_NOTE( "부동 소수점 오차 + 비선형 매핑의 정밀도 문제로 인해서 z = far 일때 변환된 z = 1 이 되지 않는다.     |     near 값을 0으로 넣으면 z = 1 이 된다." );
+				}
+			}
+
+			LS();
+
+			return r2tm::eDoLeaveAction::Pause;
+		};
+	}
+
+
+
+	r2tm::TitleFunctionT ProjectionMatrix_Perspective_Step3::GetTitleFunction() const
+	{
+		return []()->const char*
+		{
+			return "Projection Matrix : Perspective : Step 3";
+		};
+	}
+	r2tm::DoFunctionT ProjectionMatrix_Perspective_Step3::GetDoFunction() const
+	{
+		return []()->r2tm::eDoLeaveAction
+		{
+			LS();
+
+			{
+				OUTPUT_SUBJECT( "REF : perspectiveRH_ZO : https://github.com/g-truc/glm/blob/2d4c4b4dd31fde06cfffad7915c2b3006402322f/glm/ext/matrix_clip_space.inl" );
+
+				LF();
+
+				OUTPUT_SUBJECT( "perspectiveRH_ZO" );
+				OUTPUT_COMMENT( "Perspective : 원근 투영     |     -1 ~ +1 : 로 표현되는 좌표계로 변환, z = 0 ~ 1     |     fovY : 수직 시야각     |     aspect : 뷰포트의 너비 / 높이" );
+			}
+
+			LS();
+
+			{
+				OUTPUT_SOURCE_READY_N_BEGIN;
+				const float viewport_w = 400;
+				const float viewport_h = 300;
+				const float fovY = Deg2Rad( 90.f );
+				const float aspect = viewport_w / viewport_h;
+				const float near = 10.f;
+				const float far = 100.f;
+
+				const float tanHalfFovY = std::tan( fovY / 2 );
+
+				const Mat44 projection_mat4(
+					  1 / ( aspect * tanHalfFovY )  , 0.f              , 0.f                   , 0.f
+					, 0.f                           , 1 / tanHalfFovY  , 0.f                   , 0.f
+					, 0.f                           , 0.f              , far / ( near - far )  , -( far * near ) / ( far - near )
+					, 0.f                           , 0.f              , -1.f                  , 0.f
 				);
 				OUTPUT_SOURCE_END;
 
